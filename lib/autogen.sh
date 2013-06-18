e9e4b7e63183fe591a46e95bd46eaef6959ac0f3
Narate Taerat <narate@ogc.us>
2013-06-18 14:17:59 -0500

    Use '=' instead of '==' to suppress warning.
    bash give some warnings when use '==' to compare strings. Using '=' successfully
    eliminate the warnings.

--- lib/autogen.sh
+++ lib/autogen.sh
@@ -1,15 +1,15 @@
-#! /bin/sh
+#!/bin/sh
 
 # create config dir if not exist
 test -d config || mkdir config
 
 set -x
 aclocal -I config
-# On MAC OS X, GNU libtoolize is named 'glibtoolize': 
-if [ `(uname -s) 2>/dev/null` == 'Darwin' ]
-then 
-	LIBTOOLIZE=glibtoolize 
-else 
+# On MAC OS X, GNU libtoolize is named 'glibtoolize':
+if [ `(uname -s) 2>/dev/null` = 'Darwin' ]
+then
+	LIBTOOLIZE=glibtoolize
+else
 	LIBTOOLIZE=libtoolize
 fi
 ${LIBTOOLIZE} --force --copy
