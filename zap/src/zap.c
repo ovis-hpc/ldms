aee7e0c25e1ec7c66cf956470f27d563045cf287
Tom Tucker <tom@ogc.us>
2013-06-30 12:37:23 -0500

    A generic RDMA capable transport layer
    These files add an RDMA capable transport API to be used by OVIS daemons to
    communicate with each other.
    
    Change-Id: I177570568e2b31008eaaf1ef0cb3de0f41a4f66c
    Signed-off-by: Tom Tucker <tom@ogc.us>

---
+++ zap/src/zap.c
@@ -0,0 +1,305 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2013 Open Grid Computing, Inc. All rights reserved.
+ * Copyright (c) 2013 Sandia Corporation. All rights reserved.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+ * license for use of this work by or on behalf of the U.S. Government.
+ * Export of this program may require a license from the United States
+ * Government.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <sys/errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/queue.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <dlfcn.h>
+#include <assert.h>
+#include <time.h>
+#include <limits.h>
+#include <fcntl.h>
+#include "zap.h"
+#include "zap_priv.h"
+
+static void default_log(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stdout, fmt, ap);
+	fflush(stdout);
+}
+
+#if 0
+#define TF() default_log("%s:%d\n", __FUNCTION__, __LINE__)
+#else
+#define TF()
+#endif
+
+LIST_HEAD(zap_list, zap) zap_list;
+
+pthread_mutex_t zap_list_lock;
+
+#define ZAP_LIBPATH_DEFAULT "/usr/local/lib"
+#define _SO_EXT ".so"
+static char _libdir[PATH_MAX];
+
+zap_err_t zap_get(const char *name, zap_t *pz, zap_log_fn_t log_fn,
+		  zap_mem_info_fn_t mem_info_fn)
+{
+	zap_err_t ret = 0;
+	char *libdir;
+	zap_t z = NULL;
+	char *errstr;
+	int len;
+
+	if (!log_fn)
+		log_fn = default_log;
+
+	libdir = getenv("ZAP_LIBPATH");
+	if (!libdir || libdir[0] == '\0')
+		strcpy(_libdir, ZAP_LIBPATH_DEFAULT);
+	else
+		strcpy(_libdir, libdir);
+
+	/* Add a trailing / if one is not present in the path */
+	len = strlen(_libdir);
+	if (_libdir[len-1] != '/')
+		strcat(_libdir, "/");
+
+	strcat(_libdir, "libzap_");
+	strcat(_libdir, name);
+	strcat(_libdir, _SO_EXT);
+	void *d = dlopen(_libdir, RTLD_NOW);
+	if (!d) {
+		/* The library doesn't exist */
+		log_fn("dlopen: %s\n", dlerror());
+		ret = ZAP_ERR_TRANSPORT;
+		goto err;
+	}
+	dlerror();
+	zap_get_fn_t get = dlsym(d, "zap_transport_get");
+	errstr = dlerror();
+	if (errstr || !get) {
+		log_fn("dlsym: %s\n", errstr);
+		/* The library exists but doesn't export the correct
+		 * symbol and is therefore likely the wrong library type */
+		ret = ZAP_ERR_TRANSPORT;
+		goto err;
+	}
+	ret = get(pz, log_fn, mem_info_fn);
+	if (ret)
+		goto err;
+	z = *pz;
+	strcpy(z->name, name);
+	z->log_fn = log_fn;
+	z->mem_info_fn = mem_info_fn;
+
+	pthread_mutex_lock(&zap_list_lock);
+	LIST_INSERT_HEAD(&zap_list, z, zap_link);
+	pthread_mutex_unlock(&zap_list_lock);
+
+	return ZAP_ERR_OK;
+ err:
+	if (z)
+		free(z);
+	return ret;
+}
+
+size_t zap_max_msg(zap_t z)
+{
+	return z->max_msg;
+}
+
+zap_err_t zap_new(zap_t z, zap_ep_t *pep, zap_cb_fn_t cb)
+{
+	zap_err_t zerr = z->new(z, pep, cb);
+	if (!zerr) {
+		(*pep)->z = z;
+		(*pep)->cb = cb;
+		(*pep)->ref_count = 1;
+	}
+	return zerr;
+}
+
+zap_err_t zap_accept(zap_ep_t ep, zap_cb_fn_t cb)
+{
+	return ep->z->accept(ep, cb);
+}
+
+zap_err_t zap_connect(zap_ep_t ep, struct sockaddr *sa, socklen_t sa_len)
+{
+	return ep->z->connect(ep, sa, sa_len);
+}
+
+zap_err_t zap_listen(zap_ep_t ep, struct sockaddr *sa, socklen_t sa_len)
+{
+	return ep->z->listen(ep, sa, sa_len);
+}
+
+zap_err_t zap_close(zap_ep_t ep)
+{
+	return ep->z->close(ep);
+}
+
+zap_err_t zap_send(zap_ep_t ep, void *buf, size_t sz)
+{
+	return ep->z->send(ep, buf, sz);
+}
+
+zap_err_t zap_write(zap_ep_t ep,
+		    zap_map_t src_map, void *src,
+		    zap_map_t dst_map, void *dst,
+		    size_t sz,
+		    void *context)
+{
+	return ep->z->write(ep, src_map, src, dst_map, dst, sz, context);
+}
+
+zap_err_t zap_get_name(zap_ep_t ep, struct sockaddr *local_sa,
+		       struct sockaddr *remote_sa, socklen_t *sa_len)
+{
+	return ep->z->get_name(ep, local_sa, remote_sa, sa_len);
+}
+
+void zap_get_ep(zap_ep_t ep)
+{
+	pthread_mutex_lock(&ep->lock);
+	ep->ref_count++;
+	pthread_mutex_unlock(&ep->lock);
+}
+
+void zap_put_ep(zap_ep_t ep)
+{
+	int destroy = 0;
+	pthread_mutex_lock(&ep->lock);
+	assert(ep->ref_count);
+	ep->ref_count--;
+	if (!ep->ref_count)
+		destroy = 1;
+	pthread_mutex_unlock(&ep->lock);
+	if (!destroy)
+		return;
+
+	switch (ep->state) {
+	case ZAP_EP_INIT:
+	case ZAP_EP_ERROR:
+		ep->z->destroy(ep);
+		break;
+	default:
+		assert(0);
+	}
+}
+
+zap_err_t zap_read(zap_ep_t ep,
+		   zap_map_t src_map, void *src,
+		   zap_map_t dst_map, void *dst,
+		   size_t sz,
+		   void *context)
+{
+	if (dst_map->type != ZAP_MAP_LOCAL)
+		return ZAP_ERR_INVALID_MAP_TYPE;
+	if (src_map->type != ZAP_MAP_REMOTE)
+		return ZAP_ERR_INVALID_MAP_TYPE;
+
+	return ep->z->read(ep, src_map, src, dst_map, dst, sz, context);
+}
+
+
+size_t zap_map_len(zap_map_t map)
+{
+	return map->len;
+}
+
+void* zap_map_addr(zap_map_t map)
+{
+	return map->addr;
+}
+
+zap_err_t zap_map(zap_ep_t ep, zap_map_t *pm,
+		  void *addr, size_t len, zap_access_t acc)
+{
+	zap_map_t map;
+	zap_err_t err = ep->z->map(ep, pm, addr, len, acc);
+	if (err)
+		goto out;
+
+	map = *pm;
+	map->type = ZAP_MAP_LOCAL;
+	map->ep = ep;
+	map->addr = addr;
+	map->len = len;
+	map->acc = acc;
+	pthread_mutex_lock(&ep->lock);
+	LIST_INSERT_HEAD(&ep->map_list, map, link);
+	pthread_mutex_unlock(&ep->lock);
+ out:
+	return err;
+}
+
+zap_err_t zap_unmap(zap_ep_t ep, zap_map_t map)
+{
+	pthread_mutex_lock(&ep->lock);
+	LIST_REMOVE(map, link);
+	pthread_mutex_unlock(&ep->lock);
+
+	return ep->z->unmap(ep, map);
+}
+
+zap_err_t zap_share(zap_ep_t ep, zap_map_t map, uint64_t ctxt)
+{
+	return ep->z->share(ep, map, ctxt);
+}
+
+void __attribute__ ((constructor)) cs_init(void)
+{
+	pthread_mutex_init(&zap_list_lock, 0);
+}
+
+void __attribute__ ((destructor)) cs_term(void)
+{
+}
---
+++ zap/src/zap_priv.h
@@ -0,0 +1,160 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2013 Open Grid Computing, Inc. All rights reserved.
+ * Copyright (c) 2013 Sandia Corporation. All rights reserved.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+ * license for use of this work by or on behalf of the U.S. Government.
+ * Export of this program may require a license from the United States
+ * Government.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __ZAP_PRIV_H__
+#define __ZAP_PRIV_H__
+#include <inttypes.h>
+#include <sys/queue.h>
+
+#include "config.h"
+
+typedef enum zap_ep_state {
+	ZAP_EP_INIT = 0,
+	ZAP_EP_LISTENING,
+	ZAP_EP_CONNECTING,
+	ZAP_EP_CONNECTED,
+	ZAP_EP_DISCONNECTING,
+	ZAP_EP_DISCONNECTED,
+	ZAP_EP_ERROR
+} zap_ep_state_t;
+
+struct zap_ep {
+	zap_t z;
+	int ref_count;
+	pthread_mutex_t lock;
+	zap_ep_state_t state;
+
+	LIST_HEAD(zap_map_list, zap_map) map_list;
+
+	/** Event callback routine. */
+	zap_cb_fn_t cb;
+};
+
+struct zap {
+	char name[ZAP_MAX_TRANSPORT_NAME_LEN];
+	int max_msg;		/* max send message size */
+
+	LIST_ENTRY(zap) zap_link;
+
+	/** Create a new endpoint */
+	zap_err_t (*new)(zap_t z, zap_ep_t *pep, zap_cb_fn_t cb);
+
+	/** Destroy an endpoint. */
+	void (*destroy)(zap_ep_t ep);
+
+	/** Request a connection with a server */
+	zap_err_t (*connect)(zap_ep_t ep, struct sockaddr *sa, socklen_t sa_len);
+
+	/** Listen for incoming connection requests */
+	zap_err_t (*listen)(zap_ep_t ep, struct sockaddr *sa, socklen_t sa_len);
+
+	/** Accept a connection request */
+	zap_err_t (*accept)(zap_ep_t ep, zap_cb_fn_t cb);
+
+	/** Reject a connection request */
+	zap_err_t (*reject)(zap_ep_t ep);
+
+	/** Close the connection */
+	zap_err_t (*close)(zap_ep_t ep);
+
+	/** Send a message */
+	zap_err_t (*send)(zap_ep_t ep, void *buf, size_t sz);
+
+	/** RDMA write data to a remote buffer */
+	zap_err_t (*write)(zap_ep_t ep,
+			   zap_map_t src_map, void *src,
+			   zap_map_t dst_map, void *dst, size_t sz,
+			   void *context);
+
+	/**  RDMA read data from a remote buffer */
+	zap_err_t (*read)(zap_ep_t ep,
+			  zap_map_t src_map, void *src,
+			  zap_map_t dst_map, void *dst, size_t sz,
+			  void *context);
+
+	/** Allocate a remote buffer */
+	zap_err_t (*map)(zap_ep_t ep, zap_map_t *pm, void *addr, size_t len,
+			 zap_access_t acc);
+
+	/** Free a remote buffer */
+	zap_err_t (*unmap)(zap_ep_t ep, zap_map_t map);
+
+	/** Share a mapping with a remote peer */
+	zap_err_t (*share)(zap_ep_t ep, zap_map_t m, uint64_t ctxt);
+
+
+	/** Get the local and remote sockaddr for the endpoint */
+	zap_err_t (*get_name)(zap_ep_t ep, struct sockaddr *local_sa,
+			      struct sockaddr *remote_sa, socklen_t *sa_len);
+
+	/** Transport message logging callback */
+	zap_log_fn_t log_fn;
+
+	/** Memory information callback */
+	zap_mem_info_fn_t mem_info_fn;
+
+	/** Pointer to the transport's private data */
+	void *private;
+};
+
+struct zap_map {
+	LIST_ENTRY(zap_map) link; /*! List of maps for an endpoint. */
+	zap_map_type_t type;	  /*! Is this a local or remote map  */
+	zap_ep_t ep;		  /*! The endpoint */
+	zap_access_t acc;	  /*! Access rights */
+	void *addr;		  /*! Address of buffer. */
+	size_t len;		  /*! Length of the buffer */
+};
+
+typedef zap_err_t (*zap_get_fn_t)(zap_t *pz, zap_log_fn_t log_fn,
+				  zap_mem_info_fn_t map_info_fn);
+
+#endif
