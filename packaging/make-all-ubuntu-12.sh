fa7ba4db402070d119d99f6dabefca10dc4c86cb
Ben Allan <baallan@sandia.gov>
2013-12-04 11:43:10 -0800

    Fixed gpcd configure for noncray platforms.
    Switch to testing on the right variable in the safe way
    and removed gentile dir flag from makefile.am.

--- packaging/make-all-ubuntu-12.sh
+++ packaging/make-all-ubuntu-12.sh
@@ -8,7 +8,7 @@ if test -f lib/packaging/ovis-lib-toss.spec.in; then
 	expected_ovislib_prefix=$prefix
 	expected_sos_prefix=$prefix
 
-	allconfig="--prefix=$prefix --enable-rdma --enable-ssl --enable-zaptest --enable-swig --with-ovis-lib=$expected_ovislib_prefix --enable-rdma --enable-ncsa-unified --enable-sos --with-sos=$expected_sos_prefix --with-ovis-prefix=$expected_ovislib_prefix --disable-dependency-tracking "
+	allconfig="--prefix=$prefix --enable-rdma --enable-ssl --enable-zaptest --enable-swig --with-ovis-lib=$expected_ovislib_prefix --enable-sos --with-sos=$expected_sos_prefix --with-ovis-prefix=$expected_ovislib_prefix --disable-dependency-tracking "
 
 
 	if test -f $expected_event2_prefix/include/event2/event.h; then
@@ -25,7 +25,7 @@ if test -f lib/packaging/ovis-lib-toss.spec.in; then
 	mkdir lib ldms sos
 	(cd lib; ../../lib/configure $allconfig && make  && make install) && \
 	(cd sos; ../../sos/configure  $allconfig && make  && make install) && \
-	cd ldms && LDFLAGS="-L$(HOME)/opt/ovis/lib" ../../ldms/configure $allconfig && make  && make install
+	cd ldms && LDFLAGS="-L$HOME/opt/ovis/lib" ../../ldms/configure $allconfig && make  && make install
 else
 	echo "this must be run from the top of ovis source tree"
 	exit 1
