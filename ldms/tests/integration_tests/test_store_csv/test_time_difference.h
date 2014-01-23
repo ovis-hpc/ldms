a9cb3289db651ac973b30ffe817fc762f0bcefe2
ejwalsh <ejwalsh@sandia.gov>
2014-01-23 07:30:31 -0700

    changed name of variables, functions, files, folders from camel case to underscore (e.g. oneTwo to one_two)

---
+++ ldms/tests/integration_tests/test_store_csv/test_time_difference.h
@@ -0,0 +1,13 @@
+#ifndef TEST_TIME_DIFFERENCE_H_
+#define TEST_TIME_DIFFERENCE_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+int test_time_difference(char *filename, int comp_id, double time_period);
+
+
+
+#endif /*TEST_TIME_DIFFERENCE_H_ */
