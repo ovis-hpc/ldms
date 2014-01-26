b26d3853dd276fdeca400b43df383bccabe15a12
Jim Brandt <brandt@sandia.gov>
2014-01-26 15:43:28 -0800

    Add timeout on socket connect

--- ldms/src/xprt/ldms_ugni.c
+++ ldms/src/xprt/ldms_ugni.c
@@ -302,12 +302,15 @@ static int ugni_xprt_connect(struct ldms_xprt *x,
 	struct sockaddr_storage ss;
 	int cq_depth;
 	char *cq_depth_s;
-	int rc;
+	struct timeval tv;
 	gni_return_t grc;
 
 	gxp->sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (gxp->sock < 0)
 		return -1;
+	int rc = __set_socket_options(gxp);
+	if (rc)
+		goto err;
 	gxp->type = LDMS_UGNI_ACTIVE;
 
 	if (!ugni_gxp.dom.src_cq) {
@@ -340,10 +343,24 @@ static int ugni_xprt_connect(struct ldms_xprt *x,
 	pthread_mutex_unlock(&ugni_lock);
 	if (grc != GNI_RC_SUCCESS)
 		goto err;
-
+	fcntl(gxp->sock, F_SETFL, O_NONBLOCK);
 	rc = connect(gxp->sock, sa, sa_len);
-	if (rc)
+	if (errno != EINPROGRESS)
 		goto err1;
+        FD_ZERO(&fdset);
+        FD_SET(gxp->sock, &fdset);
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+        if (select(gxp->sock + 1, NULL, &fdset, NULL, &tv) == 1) {
+                int so_error;
+                socklen_t len = sizeof so_error;
+                getsockopt(gxp->sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
+                if (so_error)
+                        goto err1;
+        } else
+                goto err1;
+        fcntl(gxp->sock, F_SETFL, ~O_NONBLOCK);
+
 	sa_len = sizeof(ss);
 	rc = getsockname(gxp->sock, (struct sockaddr *)&ss, &sa_len);
 	if (rc)
