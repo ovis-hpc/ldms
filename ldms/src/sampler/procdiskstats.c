81ff4b85fabf557cc9fa65c65dc07c74e064f27f
Nichamon Naksinehaboon <nichamon@ogc.us>
2014-03-28 15:03:23 -0500

    ldms: procdiskstats: Add the attribute 'device'
     - The attribute 'device' allows users to specify the devices of which the
       metrics will be collected.
    
    Change-Id: I5feb3bfde727220699e39e2b268f0cc679c3349c

--- ldms/src/sampler/procdiskstats.c
+++ ldms/src/sampler/procdiskstats.c
@@ -21,6 +21,9 @@ static char *fieldname[NFIELD] =
 	"writes_comp","writes_merg", "sect_written", "time_write",
 	"ios_in_progress", "time_ios", "weighted_time"};
 
+static char **devices;
+static int ndevices;
+
 ldms_set_t set;
 FILE *mf;
 ldms_metric_t *metric_table;
@@ -37,8 +40,8 @@ static int create_metric_set(const char *path)
 	char lbuf[256];
 	char metric_name[128];
 	char name[64];
-	int i, junk1, junk2;
-	int num_device = 0;
+	int i, j, junk1, junk2;
+	int nfound_device = 0;
 	uint64_t temp_comp_id = comp_id;
 	ldms_metric_t m;
 
@@ -70,18 +73,52 @@ static int create_metric_set(const char *path)
 		if (rc != 14)
 			break;
 
-		for (i = 0; i < NFIELD; i++) {
-			snprintf(metric_name, 128, "%s#%s", fieldname[i], name);
-			rc = ldms_get_metric_size(metric_name,
-					LDMS_V_U64, &meta_sz, &data_sz);
+		if (ndevices > 0) {
 
-			if (rc)
-				return rc;
+			if (nfound_device == ndevices)
+				/* Found all specified devices */
+				continue;
 
-			tot_meta_sz += meta_sz;
-			tot_data_sz += data_sz;
-			metric_count++;
+			for (j = 0; j < ndevices; j++) {
+				if (0 == strcmp(devices[j], name)) {
+					for (i = 0; i < NFIELD; i++) {
+						snprintf(metric_name, 128,
+							"%s#%s", fieldname[i],
+									name);
+						rc = ldms_get_metric_size(
+								metric_name,
+								LDMS_V_U64,
+								&meta_sz,
+								&data_sz);
+
+						if (rc)
+							return rc;
+
+						tot_meta_sz += meta_sz;
+						tot_data_sz += data_sz;
+						metric_count++;
+					}
+					nfound_device++;
+					break;
+				}
+			}
+		} else {
+			for (i = 0; i < NFIELD; i++) {
+				snprintf(metric_name, 128, "%s#%s",
+						fieldname[i], name);
+				rc = ldms_get_metric_size(metric_name,
+						LDMS_V_U64, &meta_sz,
+							&data_sz);
+
+				if (rc)
+					return rc;
+
+				tot_meta_sz += meta_sz;
+				tot_data_sz += data_sz;
+				metric_count++;
+			}
 		}
+
 	} while (s);
 
 	rc = ldms_create_set(path, tot_meta_sz, tot_data_sz, &set);
@@ -96,6 +133,7 @@ static int create_metric_set(const char *path)
 	 * Process the file again to define all the metrics.
 	 */
 	metric_no = 0;
+	nfound_device = 0;
 
 	fseek(mf, 0, SEEK_SET);
 	do {
@@ -112,15 +150,45 @@ static int create_metric_set(const char *path)
 		if (rc != 14)
 			break;
 
-		for (i = 0; i < NFIELD; i++) {
-			snprintf(metric_name, 128, "%s#%s", fieldname[i], name);
-			m = ldms_add_metric(set, metric_name, LDMS_V_U64);
-			if (!m) {
-				rc = ENOMEM;
-				goto err;
+		if (ndevices > 0) {
+			if (nfound_device == ndevices)
+				/* Found all specified devices */
+				continue;
+
+			for (j = 0; j < ndevices; j++) {
+				if (0 == strcmp(devices[j], name)) {
+					for (i = 0; i < NFIELD; i++) {
+						snprintf(metric_name, 128,
+							"%s#%s", fieldname[i],
+									name);
+						m = ldms_add_metric(set,
+								metric_name,
+								LDMS_V_U64);
+						if (!m) {
+							rc = ENOMEM;
+							goto err;
+						}
+						ldms_set_user_data(m,
+								temp_comp_id);
+						metric_table[metric_no++] = m;
+					}
+					nfound_device++;
+					break;
+				}
 			}
-			ldms_set_user_data(m, temp_comp_id);
-			metric_table[metric_no++] = m;
+		} else {
+			for (i = 0; i < NFIELD; i++) {
+				snprintf(metric_name, 128, "%s#%s",
+						fieldname[i], name);
+				m = ldms_add_metric(set, metric_name,
+							LDMS_V_U64);
+				if (!m) {
+					rc = ENOMEM;
+					goto err;
+				}
+				ldms_set_user_data(m, temp_comp_id);
+				metric_table[metric_no++] = m;
+			}
 		}
 		temp_comp_id++;
 
@@ -133,32 +201,100 @@ static int create_metric_set(const char *path)
 
 }
 
+static int add_device(struct attr_value_list *avl)
+{
+	char *value = av_value(avl, "device");
+
+	if (!value) {
+		/*
+		 * If no devices are give, collect all devices.
+		 */
+		ndevices = -1;
+		devices = NULL;
+		return 0;
+	}
+
+	char *value_tmp = strdup(value);
+	if (!value_tmp)
+		goto enomem;
+
+	char *ptr, *token;
+	token = strtok_r(value_tmp, ",", &ptr);
+	while (token) {
+		ndevices++;
+		token = strtok_r(NULL, ",", &ptr);
+	}
+	free(value_tmp);
+
+	devices = malloc(ndevices * sizeof(char *));
+	if (!devices)
+		goto enomem;
+
+	int i, j;
+	token = strtok_r(value, ",", &ptr);
+	devices[0] = strdup(token);
+	if (!devices[0])
+		goto free_devices;
+
+	for (i = 1; i < ndevices; i++) {
+		token = strtok_r(NULL, ",", &ptr);
+		devices[i] = strdup(token);
+		if (!devices[i])
+			goto free_devices;
+	}
+
+	return 0;
+
+free_devices:
+	for (j = 0; j < i; j++)
+		free(devices[j]);
+	free(devices);
+enomem:
+	msglog("procdiskstat: add_device: Out of memory.\n");
+	return ENOMEM;
+
+}
 
 static int config(struct attr_value_list *kwl, struct attr_value_list *avl)
 {
-        char *value;
+	char *value;
+	char *attr;
+	int rc;
 
-        value = av_value(avl, "component_id");
-        if (value)
-                comp_id = strtol(value, NULL, 0);
+	rc = add_device(avl);
+	if (rc)
+		return rc;
 
-        value = av_value(avl, "set");
-        if (value)
-                create_metric_set(value);
+	attr = "component_id";
+	value = av_value(avl, attr);
+	if (value)
+		comp_id = strtol(value, NULL, 0);
+	else
+		goto enoent;
 
-        return 0;
+	attr = "set";
+	value = av_value(avl, attr);
+	if (value)
+		create_metric_set(value);
+	else
+		goto enoent;
+
+	return 0;
+enoent:
+	msglog("procdiskstat: requires '%s'\n", attr);
+	return ENOENT;
 }
 
 
 static int sample(void)
 {
-	int rc, i;
-	int metric_no;
+	int rc, i, j;
+	int index;
 	char *s;
 	char name[64];
 	char lbuf[256];
 	char metric_name[128];
-	int junk1, junk2;
+	int junk1, junk2, nfound_device;
 	union ldms_value v[NFIELD];
 
 	if (!set) {
@@ -166,7 +302,8 @@ static int sample(void)
 		return EINVAL;
 	}
 	ldms_begin_transaction(set);
-	metric_no = 0;
+	index = 0;
+	nfound_device = 0;
 	fseek(mf, 0, SEEK_SET);
 	do {
 		s = fgets(lbuf, sizeof(lbuf), mf);
@@ -185,9 +322,25 @@ static int sample(void)
 			goto out;
 		}
 
-		for (i = 0; i < NFIELD; i++)
-			ldms_set_metric(metric_table[metric_no++], &v[i]);
+		if (ndevices > 0) {
+			if (nfound_device == ndevices)
+				/* Found all specified devices */
+				continue;
 
+			for (j = 0; j < ndevices; j++) {
+				if (0 == strcmp(devices[j], name)) {
+					for (i = 0; i < NFIELD; i++)
+						ldms_set_metric(
+							metric_table[index++],
+								&v[i]);
+					nfound_device++;
+					break;
+				}
+			}
+		} else {
+			for (i = 0; i < NFIELD; i++)
+				ldms_set_metric(metric_table[index++], &v[i]);
+		}
 	} while (s);
 
 out:
@@ -214,9 +367,10 @@ static void term(void)
 
 static const char *usage(void)
 {
-        return  "config name=procdiskstats component_id=<comp_id> set=<setname>\n"
+        return  "config name=procdiskstats component_id=<comp_id> set=<setname> device=<device>\n"
                 "    comp_id     The component id value.\n"
-                "    setname     The set name.\n";
+                "    setname     The set name.\n"
+                "    device	 The comma-separated list of devices\n";
 }
 
 static struct ldmsd_sampler procdiskstats_plugin = {
