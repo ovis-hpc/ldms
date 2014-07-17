5b5c710650fe33aaf6dc88f430a2f244e054da6c
Nichamon Naksinehaboon <nichamon@ogc.us>
2014-07-17 16:29:48 -0500

    ovis: log files management
    The old log files are of the format <service name>.log.N, where N is 0, 1, ...,
    5. *.log.5 is the oldest log file kept for a service. For example,
    ldmsd_sampler.log, ldmsd_sampler.log.0, ..., ldmsd_sampler.log.5.
    
    Change-Id: I78096aa767ff658c400cb157a74c3fed7113edbd

--- lib/etc/ovis/ovis-functions.sh
+++ lib/etc/ovis/ovis-functions.sh
@@ -6,3 +6,17 @@ get_pid() {
 	ps -C $base h -o pid,cmd | grep $full |
 		sed 's/ *\([0-9]\+\) .*/\1/'
 }
+
+archive_log() {
+	log=$1
+	local x
+	for x in $(seq 4 -1 0); do
+		if [ -e $log.$x ] ; then
+			mv $log.$x $log.$(($x+1))
+		fi
+	done
+
+	if [ -e $log ] ; then
+		mv $log $log.0
+	fi
+}
