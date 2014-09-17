efad216ca53ac3fa41d1cd84c956c07069ea9714
Ben Allan <baallan@sandia.gov>
2014-09-17 16:11:02 -0700

    ENH: cleaner tar building and relaxing autotools
    release tars need the build scripts generated so the users don't need
    our exact version of autotools to build on most platforms.

--- util/release_tarball.sh
+++ util/release_tarball.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
-REPO_DIR=/home/brandt/Source/ovispublic_hekili
-OUTPUT_DIR=/home/brandt/ldms_release
-TMP_DIR=/home/brandt/ldms_release_tmp
+if ! test -f m4/Ovis-top.m4; then
+	echo This must be run from top of a source tree, which should be clean.
+	exit 1
+fi
+REPO_DIR=`pwd`
+OUTPUT_DIR=`pwd`/ldms_release
 BRANCH_NAME=ovispublic_RC1.2_a
 # Will get this from git in the future
 VERSION=2.2.0
 
-
 # Create output dir
 mkdir -p $OUTPUT_DIR
-
-# Create tmp dir
-mkdir -p $TMP_DIR
 
 # cd to local git clone
 cd $REPO_DIR
@@ -26,9 +25,11 @@ COMMIT_ID="$(git log -1 --pretty="%H")"
 # Get most recent tag id for this branch
 TAG_ID="$(git describe --tags --abbrev=0)"
 
+TARGET=ldms-${VERSION}.tar
+
 # Create archive of desired branch
 #git archive $BRANCH_NAME --format=tar --output=${OUTPUT_DIR}/ldms-${VERSION}.tar
-git archive $COMMIT_ID --format=tar --output=${OUTPUT_DIR}/ldms-${VERSION}.tar
+git archive --prefix=ldms-${VERSION}/ $COMMIT_ID --format=tar --output=${OUTPUT_DIR}/$TARGET
 sleep 0.1
 
 # cd to output dir
@@ -36,42 +37,26 @@ cd $OUTPUT_DIR
 
 # Untar archive
 echo "Untarring archive"
-tar xf ldms-${VERSION}.tar
+tar xf $TARGET
 sleep 0.1
 
 # Add SHA file
+pushd ldms-${VERSION}
 echo $COMMIT_ID > SHA.txt
 echo $TAG_ID > TAG.txt
+./autogen.sh
+popd
 
 # Tar back up excluding unwanted files and dirs
-echo "tarring archive with excludes"
-tar cf ldms-${VERSION}.tar \
-	--exclude='ldms/src/sampler/sedc.c' \
-	--exclude='ldms/src/store/store_sos.c' \
-	--exclude='ldms/src/store/store_mysql.c' \
-	--exclude='ldms/src/store/store_mysqlbulk.c' \
-	--exclude='sos' \
-	--exclude='lib/src/zap' \
-	--exclude='util/release_tarball.sh' \
-	*
-sleep 0.1
-# Move archive to tmp dir
-echo "Moving tarball to tmp"
-mv ldms-${VERSION}.tar $TMP_DIR
+echo "tarring archive with excludes from "
+echo "$REPO_DIR/util/tar-excludes.txt"
+TAR_OPTS="-X $REPO_DIR/util/tar-excludes.txt"
+tar cf $TARGET $TAR_OPTS ldms-${VERSION}
 sleep 0.1
 
 # Remove untarred stuff
 echo "Removing cruft"
-rm -rf *
+rm -rf ldms-${VERSION}
 sleep 0.1
-
-# Move archive from tmp to release dir
-echo "Moving archive back to release dir"
-mv $TMP_DIR/ldms-${VERSION}.tar .
-sleep 0.1
-
-# Remove tmp dir
-echo "Removing tmp dir"
-rmdir $TMP_DIR
 
 ls -l
