c7aea4b4af756af95fe0b60e283070d7327be18b
Tom Tucker <tom@ogc.us>
2015-06-12 17:57:58 -0500

    Fix incorrect memory size in context allocation
    The containing plugin structure's context type is void*,  however
    the type it points to is a plugin specific structure. So the allocation
    that uses sizeof(*p->context) will always allocate sizeof(void*),
    not sizeof(struct bin_metric_ctxt) or whatever it happens to
    be. This results in memory corruption.
    
    Change-Id: I908a729414ee7ac5cd79a738fe8be321ad2e3090
    Signed-off-by: Tom Tucker <tom@ogc.us>

--- baler/src/plugins/bin_metric.c
+++ baler/src/plugins/bin_metric.c
@@ -499,7 +499,7 @@ struct bplugin* create_plugin_instance()
 	p->start = plugin_start;
 	p->stop = plugin_stop;
 	p->free = plugin_free;
-	struct bin_metric_ctxt *ctxt = calloc(1, sizeof(*p->context));
+	struct bin_metric_ctxt *ctxt = calloc(1, sizeof(*ctxt));
 	ctxt->status = PSTATUS_STOPPED;
 	ctxt->port = BIN_METRIC_DEFAULT_PORT;
 	p->context = ctxt;
