5ca42cfc7ff5a55468490df2a36f2972cab4e37f
Nichamon Naksinehaboon <nichamon@ogc.us>
2015-06-18 14:53:52 -0500

    ldmsd python test: Added ldmsd_test package and the testing config file
    The ldmsd_test_config.py in the ldmsd_test_config directory contains the
    variables needed in ldmsd testing.
    In the ldmsd_test package,
     - ldmsd_test_util.py: a module providing convenient methods for implement test
       cases
     - conftest.py: The main py.test configuration file for the ldmsd testing
    
    Change-Id: Ie97a8c899db8ee95954b460eced11616511d7f83

---
+++ ldms/python/ldmsd_test/conftest.py
@@ -0,0 +1,168 @@
+#######################################################################
+# -*- c-basic-offset: 8 -*-
+# Copyright (c) 2015 Open Grid Computing, Inc. All rights reserved.
+# Copyright (c) 2015 Sandia Corporation. All rights reserved.
+# Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+# license for use of this work by or on behalf of the U.S. Government.
+# Export of this program may require a license from the United States
+# Government.
+#
+# This software is available to you under a choice of one of two
+# licenses.  You may choose to be licensed under the terms of the GNU
+# General Public License (GPL) Version 2, available from the file
+# COPYING in the main directory of this source tree, or the BSD-type
+# license below:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#      Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#      Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+#      Neither the name of Sandia nor the names of any contributors may
+#      be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+#
+#      Neither the name of Open Grid Computing nor the names of any
+#      contributors may be used to endorse or promote products derived
+#      from this software without specific prior written permission.
+#
+#      Modified source versions must be plainly marked as such, and
+#      must not be misrepresented as being the original software.
+#
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#######################################################################
+'''
+Created on May 27, 2015
+
+@author: nichamon
+'''
+
+# py.test version 2.5.1
+
+import pytest
+import logging
+from ldmsd.ldmsd_setup import start_ldmsd, is_ldmsd_running, kill_ldmsd,\
+    kill_9_ldmsd
+from ovis_ldms.ldms import ldms_init
+from ldmsd.ldmsd_util import remove_file, get_var_from_file
+
+def __second_2_microsecond(sec):
+    return sec * 1000000
+
+def pytest_addoption(parser):
+    parser.addoption("--pre-mem", action="store", default=1024,
+                     help = "Pre-allocated memory size by ldms")
+    levels_list = [logging.DEBUG, logging.INFO, logging.ERROR]
+    parser.addoption("--verbosity", action = "store",
+                     choices = [logging._levelNames[level] for level in levels_list],
+                     default = logging._levelNames[logging.INFO],
+                     help = "Verbosity of logging")
+    parser.addoption("--config-file", action = "store",
+                     help = "Path to the ldmsd test config file.")
+
+def pytest_configure(config):
+    path = config.getoption("--config-file")
+    cfg = get_var_from_file("cfg", path)
+    for host_list in [cfg.AGG_HOSTS, cfg.AGG2_HOSTS, cfg.STORED_HOSTS]:
+        if "localhost" not in host_list:
+            host_list += ["localhost"]
+
+    cfg.LDMSD_RECONNECT_INTERVAL = __second_2_microsecond(cfg.LDMSD_RECONNECT_INTERVAL)
+    pytest.cfg = cfg
+
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed {0}".format(previousfailed.name))
+
+###########################################
+# logging
+###########################################
+logging.basicConfig(level = logging.INFO,
+                    format = '\n%(levelname)s: %(message)s')
+
+@pytest.fixture(scope = "session", autouse = True)
+def logger(request):
+    level_s = request.config.getoption("--verbosity")
+    level = logging._levelNames[level_s]
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    return logger
+
+#############################################
+# Get the config variables.
+#############################################
+
+@pytest.fixture(scope = "session", autouse = True)
+def cfg(request, logger):
+    return pytest.cfg
+
+###########################################
+# Session, modules and test cases divider
+###########################################
+@pytest.fixture(scope = "session", autouse = True)
+def divider_session(request):
+    logging.info("############### session start ###############")
+    def fin():
+        logging.info("############### session done ###############")
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope = "module", autouse = True)
+def divider_module(request):
+    logging.info("########## module '{0}' start ##########".format(
+                                                request.module.__name__))
+    def fin():
+        logging.info("########## module '{0}' done ##########".format(
+                                                    request.module.__name__))
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope = "class", autouse = True)
+def divider_class(request):
+    logging.info("############### class '{0}' start ################".format(
+                                                request.cls.__name__))
+    def fin():
+        logging.info("############### class '{0}' done ################".format(
+                                                    request.cls.__name__))
+    request.addfinalizer(fin)
+
+@pytest.fixture(scope = "function", autouse = True)
+def divider_function(request):
+    logging.info("##### {0} start#####".format(
+                                                request.function.__name__))
+    def fin():
+        logging.info("##### {0} done#####".format(
+                                                    request.function.__name__))
+    request.addfinalizer(fin)
+
+###########################################
+# Things that really matter
+###########################################
+@pytest.fixture(scope = "session", autouse = True)
+def ldmsd_test_init(request):
+    size = request.config.getoption("--pre-mem")
+    ldms_init(size)
---
+++ ldms/python/ldmsd_test_config/ldmsd_test_config.py
@@ -0,0 +1,165 @@
+"""Configuration of ldmsd Tests
+
+This file contains the required variables in the tests.
+"""
+
+SAMPLERD_OVIS = "/home/nichamon/${HOSTNAME}"
+AGG_OVIS = "/home/nichamon/${HOSTNAME}"
+
+LOG_HOME = "/var/log/ovis"
+SOCK_HOME = "/var/run/ovis"
+STORE_HOME = "/etc/store/"
+
+#==================================================================
+# DO NOT DELETE/RENAME THE VARIABLES AFTER THIS POINT
+#
+# TODO for testers: Please assign values to the following variables
+#==================================================================
+
+#-------------------------------------------
+# BEGIN: Test environment-specific variables
+#-------------------------------------------
+
+#=========== List of hosts that will run samplerd(s)
+SAMPLERD_HOSTS = map(lambda i: "orion-0{0}".format(i), [5, 6]) + ['localhost']
+
+#=========== Transport used by the samplerd(s)
+SAMPLERD_XPRT = "sock"
+
+#=========== Listener ports used by the samplerd(s)
+SAMPLERD_PORT = 10001
+
+#=========== Log file path of the samplerd(s). Bash string extension is valid
+SAMPLERD_LOG = SAMPLERD_OVIS + LOG_HOME + "/ldmsd_sampler.log"
+
+#=========== Socket path of the samplerd(s). Bash string extension is valid
+SAMPLERD_SOCK = SAMPLERD_OVIS + SOCK_HOME + "/ldmsd_sampler.sock"
+
+#=========== samplerd's listener ports for inet ctrl configuration
+SAMPLERD_INET_CTRL_PORT = 20001
+
+#===========hostnames of aggregators
+# "localhost" must be in the list.
+AGG_HOSTS = ["localhost", "orion-05"]
+#AGG_HOSTS = ["localhost"]
+
+#===========Transport used by the aggregators
+AGG_XPRT = "sock"
+
+#===========Listener port of the aggregators
+AGG_PORT = 10002
+
+#===========Log file path of the aggregators
+AGG_LOG = AGG_OVIS + LOG_HOME + "/ldmsd_agg.log"
+
+#===========Socket path of the aggregators
+AGG_SOCK = AGG_OVIS + SOCK_HOME + "/ldmsd_agg.sock"
+
+#===========Listener ports for python-remote configuration of the aggregators
+AGG_INET_CTRL_PORT = 20002
+
+#===========hostnames of the 2nd-level aggregators
+# "localhost" must be in the list.
+AGG2_HOSTS = ["orion-05"]
+
+#===========Transport used by the 2nd-level aggregators
+AGG2_XPRT = "sock"
+
+#===========Listener port of the 2nd-level aggregators
+AGG2_PORT = 10003
+
+#===========Log file path of the 2nd-level aggregators
+AGG2_LOG = AGG_OVIS + LOG_HOME + "/ldmsd_agg_2nd.log"
+
+#===========Socket path of the 2nd-level aggregators
+AGG2_SOCK = AGG_OVIS + SOCK_HOME + "/ldmsd_agg_2nd.sock"
+
+#===========Listener ports for python-remote configuration of the 2nd-level aggregators
+AGG2_INET_CTRL_PORT = 20003
+
+#######################
+# Stored are ldmsd processes that are for storing only. They don't aggregate
+# from samplerd directly. This is for testing the scenarios that
+# the aggregators and stored are different ldmsd processes.
+#
+# Hence, the listener port and the socket file must be *different* from samplerds' and
+# aggregators'listener ports and socket files.
+#######################
+
+#===========hostnames of the stored
+# "localhost" must be in the list.
+STORED_HOSTS = ["localhost"]
+
+#===========Transport used by the stored
+STORED_XPRT = "sock"
+
+#===========Listener port of the stored
+STORED_PORT = 10004
+
+#===========Log file path of the stored
+STORED_LOG = AGG_OVIS + LOG_HOME + "/ldmsd_stored.log"
+
+#===========Socket path of the stored
+STORED_SOCK = AGG_OVIS + SOCK_HOME + "/ldmsd_stored.sock"
+
+#===========Listener ports for python-remote configuration of the stored
+STORED_INET_CTRL_PORT = 20004
+
+
+#------------------------------------------
+# END: Test environment-specific variables
+#------------------------------------------
+
+
+#=========== Number of test instances on each samplerd
+NUM_TEST_INSTANCES_PER_HOST = 1
+
+#=========== Prefix-name of a test instance. Hard-coded in ldmsd. DON'T Change.
+TEST_INSTANCE_PREFIX_NAME = "test_set"  # Don't change. It is defined in ldmsd.
+
+#=========== Number of metrics in each test instance
+TEST_INSTANCE_NUM_METRICS = 50
+
+#=========== Aggregator re-connect interval. Default is 20 seconds, hard-coded in ldmsd.
+LDMSD_RECONNECT_INTERVAL = 20 # seconds
+
+#=========== Aggregator re-connect offset.
+LDMSD_RECONNECT_OFFSET = 0 # microseconds
+
+#=========== Number of samplerd per aggregator (for large-scale test)
+# Number of samplerd that will be added to an aggregator
+# If there is only one element 'n', at most 'n' samplerd will be added
+# to an aggregator, depending on the number of given samplerd hosts.
+#
+# If there are more than one elements, each number represents one test case.
+NUM_SAMPELRD_PER_AGG = [10000]
+
+#=========== List of store plugins and their configuration
+STORE_PI = {'sos': {'path': AGG_OVIS + STORE_HOME}}
+
+#=========== List of store policies
+STORE_POLICY = [
+                {'store_pi': "sos",
+                 'policy': "sos_all",
+                 'container': "test_set_all",
+                 'schema': "test_set",
+                 'metrics': None,
+                 'hosts': None
+                 },
+                {'store_pi': "sos",
+                 'policy': "sos_some",
+                 'container': "test_set_some",
+                 'schema': "test_set",
+                 'metrics': ["metric_0", "metric_1"],
+                 'hosts': None
+                 }
+                ]
+
+
+
+#-------------------------------------------
+# BEGIN: Variables related to ldmsd commands
+#-------------------------------------------
+
+#=========== Update interval given when construct add_host command.
+LDMSD_UPDATE_INTERVAL = 2 # seconds
