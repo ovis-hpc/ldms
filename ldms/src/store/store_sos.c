7bb9ca850321b08a3fcf983be49637e515a500a2
Tom Tucker <tom@ogc.us>
2015-10-06 19:01:27 -0400

    Fix the metric order in ldmsd_strgp to match the set
    The metrics were being added to the head of the list which results
    in the metric list on the storage policy being in the reverse order
    of the metrics in the set. Also, the metric list was not being passed
    to the open call in the store, so it was not possible to know what
    metrics would be in a subsequent store call.
    
    Signed-off-by: Tom Tucker <tom@ogc.us>

--- ldms/src/store/store_sos.c
+++ ldms/src/store/store_sos.c
@@ -208,7 +208,7 @@ static void *get_ucontext(ldmsd_store_handle_t _sh)
 
 static ldmsd_store_handle_t
 open_store(struct ldmsd_store *s, const char *container, const char *schema,
-	   struct ldmsd_store_metric_list *metric_list, void *ucontext)
+	   struct ldmsd_strgp_metric_list *metric_list, void *ucontext)
 {
 	struct sos_instance *si = NULL;
 
