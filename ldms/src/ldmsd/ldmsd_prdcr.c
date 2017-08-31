ad7998bb21a25aa2bf0746dacbe95dd3fc8e1447
Ben Allan <baallan@sandia.gov>
2017-08-31 11:14:01 -0600

    downgrading to info, as even warning is too loud.

--- ldms/src/ldmsd/ldmsd_prdcr.c
+++ ldms/src/ldmsd/ldmsd_prdcr.c
@@ -381,9 +381,9 @@ static void prdcr_connect(ldmsd_prdcr_t prdcr)
 		prdcr->conn_state = LDMSD_PRDCR_STATE_CONNECTING;
 #if OVIS_LIB_HAVE_AUTH
 		prdcr->xprt = ldms_xprt_with_auth_new(prdcr->xprt_name,
-				ldmsd_lwarning, ldmsd_secret_get());
+				ldmsd_linfo, ldmsd_secret_get());
 #else
-		prdcr->xprt = ldms_xprt_new(prdcr->xprt_name, ldmsd_lwarning);
+		prdcr->xprt = ldms_xprt_new(prdcr->xprt_name, ldmsd_linfo);
 #endif /* OVIS_LIB_HAVE_AUTH */
 		if (prdcr->xprt) {
 			ret  = ldms_xprt_connect(prdcr->xprt,
