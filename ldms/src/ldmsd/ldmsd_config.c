bf74e250eea4b999e7eb3ff4a787f842b8fce6de
Tom Tucker <tom@ogc.us>
2017-12-02 09:33:25 -0700

    Move a config log message to the debug level

--- ldms/src/ldmsd/ldmsd_config.c
+++ ldms/src/ldmsd/ldmsd_config.c
@@ -451,13 +451,13 @@ static int log_response_fn(ldmsd_cfg_xprt_t xprt, char *data, size_t data_len)
 	attr = ldmsd_first_attr(req_reply);
 	if (attr->discrim == 0 || attr->discrim == 1) {
 		/* We don't dump attributes to the log */
-		ldmsd_log(LDMSD_LERROR, "msg_no %d flags %x rec_len %d rsp_err %d msg\n",
+		ldmsd_log(LDMSD_LDEBUG, "msg_no %d flags %x rec_len %d rsp_err %d msg\n",
 			  req_reply->msg_no, req_reply->flags, req_reply->rec_len,
 			  req_reply->rsp_err);
 	} else {
 		data[data_len] = '\0';
 		attr = ldmsd_first_attr(req_reply);
-		ldmsd_log(LDMSD_LERROR, "msg_no %d flags %x rec_len %d rsp_err %d msg %s\n",
+		ldmsd_log(LDMSD_LDEBUG, "msg_no %d flags %x rec_len %d rsp_err %d msg %s\n",
 			  req_reply->msg_no, req_reply->flags, req_reply->rec_len,
 			  req_reply->rsp_err, attr);
 	}
