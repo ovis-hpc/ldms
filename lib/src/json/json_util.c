f890cdf6692402f7cb45f5f6fc978af0f49ef731
Tom Tucker <tom@ogc.us>
2019-03-06 08:32:49 -0600

    Implement a JSon parser service

---
+++ lib/src/json/json_util.c
@@ -0,0 +1,398 @@
+#include  <stdarg.h>
+#include <assert.h>
+#include "json_util.h"
+
+#define JSON_BUF_START_LEN 8192
+
+jbuf_t jbuf_new(void)
+{
+	jbuf_t jb = malloc(sizeof(*jb) + JSON_BUF_START_LEN);
+	if (jb) {
+		jb->buf_len = JSON_BUF_START_LEN;
+		jb->cursor = 0;
+	}
+	return jb;
+}
+
+void jbuf_free(jbuf_t jb)
+{
+	free(jb);
+}
+
+jbuf_t jbuf_append_va(jbuf_t jb, const char *fmt, va_list ap)
+{
+	int cnt, space;
+ retry:
+	space = jb->buf_len - jb->cursor;
+	cnt = vsnprintf(&jb->buf[jb->cursor], space, fmt, ap);
+	if (cnt > space) {
+		space = jb->buf_len + JSON_BUF_START_LEN;
+		jb = realloc(jb, space);
+		if (jb) {
+			jb->buf_len = space;
+			goto retry;
+		} else {
+			return NULL;
+		}
+	}
+	jb->cursor += cnt;
+	return jb;
+}
+
+jbuf_t jbuf_append_str(jbuf_t jb, const char *fmt, ...)
+{
+	int cnt, space;
+	va_list ap;
+	va_start(ap, fmt);
+	jb = jbuf_append_va(jb, fmt, ap);
+	va_end(ap);
+	return jb;
+}
+
+jbuf_t jbuf_append_attr(jbuf_t jb, const char *name, const char *fmt, ...)
+{
+	int cnt, space;
+	va_list ap;
+	va_start(ap, fmt);
+	jb = jbuf_append_str(jb, "\"%s\":", name);
+	if (jb)
+		jb = jbuf_append_va(jb, fmt, ap);
+	va_end(ap);
+	return jb;
+}
+
+json_entity_t json_attr_first(json_entity_t d)
+{
+	hent_t ent;
+	assert(d->type == JSON_DICT_VALUE);
+	ent = htbl_first(d->value.dict_->attr_table);
+	json_attr_t a = container_of(ent, struct json_attr_s, attr_ent);
+	return &a->base;
+}
+
+json_entity_t json_attr_next(json_entity_t a)
+{
+	hent_t ent;
+	json_attr_t next;
+	assert(a->type == JSON_ATTR_VALUE);
+	ent = htbl_next(&a->value.attr_->attr_ent);
+	if (ent) {
+		next = container_of(ent, struct json_attr_s, attr_ent);
+		return &next->base;
+	}
+	return NULL;
+}
+
+json_entity_t json_attr_find(json_entity_t d, char *name)
+{
+	hent_t ent;
+	json_attr_t a;
+	assert (d->type == JSON_DICT_VALUE);
+	ent = htbl_find(d->value.dict_->attr_table, name, strlen(name));
+	if (ent) {
+		a = container_of(ent, struct json_attr_s, attr_ent);
+		return &a->base;
+	}
+	return NULL;
+}
+
+int attr_cmp(const void *a, const void *b, size_t key_len)
+{
+	return strncmp(a, b, key_len);
+}
+
+#define JSON_HTBL_DEPTH	23
+
+static json_entity_t json_dict_new(void)
+{
+	json_dict_t d = malloc(sizeof *d);
+	if (d) {
+		d->base.type = JSON_DICT_VALUE;
+		d->base.value.dict_ = d;
+		d->attr_table = htbl_alloc(attr_cmp, JSON_HTBL_DEPTH);
+		if (!d->attr_table) {
+			free(d);
+			return NULL;
+		}
+		return &d->base;
+	}
+	return NULL;
+}
+
+static json_entity_t json_str_new(const char *s)
+{
+	json_str_t str = malloc(sizeof *str);
+	if (str) {
+		str->base.type = JSON_STRING_VALUE;
+		str->base.value.str_ = str;
+		str->str = strdup(s);
+		if (!str->str) {
+			free(str);
+			return NULL;
+		}
+		str->str_len = strlen(s);
+		return &str->base;
+	}
+	return NULL;
+}
+
+static json_entity_t json_list_new(void)
+{
+	json_list_t a = malloc(sizeof *a);
+	if (a) {
+		a->base.type = JSON_LIST_VALUE;
+		a->base.value.list_ = a;
+		a->item_count = 0;
+		TAILQ_INIT(&a->item_list);
+		return &a->base;
+	}
+	return NULL;
+}
+
+void json_item_add(json_entity_t a, json_entity_t e)
+{
+	assert(a->type == JSON_LIST_VALUE);
+	a->value.list_->item_count++;
+	TAILQ_INSERT_TAIL(&a->value.list_->item_list, e, item_entry);
+}
+
+json_entity_t json_item_first(json_entity_t a)
+{
+	json_entity_t i;
+	assert(a->type == JSON_LIST_VALUE);
+	i = TAILQ_FIRST(&a->value.list_->item_list);
+	return i;
+}
+
+json_entity_t json_item_next(json_entity_t a)
+{
+	return TAILQ_NEXT(a, item_entry);
+}
+
+static json_entity_t json_attr_new(json_entity_t name, json_entity_t value)
+{
+	json_attr_t a = malloc(sizeof *a);
+	if (a) {
+		a->base.type = JSON_ATTR_VALUE;
+		a->base.value.attr_ = a;
+		a->name = name;
+		a->value = value;
+		return &a->base;
+	}
+	return NULL;
+}
+
+json_entity_t json_entity_new(enum json_value_e type, ...)
+{
+	uint64_t i;
+	double d;
+	char *s;
+	va_list ap;
+	json_entity_t e, name, value;
+
+	va_start(ap, type);
+	switch (type) {
+	case JSON_INT_VALUE:
+		e = malloc(sizeof *e);
+		if (!e)
+			goto out;
+		e->type = type;
+		i = va_arg(ap, uint64_t);
+		e->value.int_ = i;
+		break;
+	case JSON_BOOL_VALUE:
+		e = malloc(sizeof *e);
+		if (!e)
+			goto out;
+		e->type = type;
+		i = va_arg(ap, int);
+		e->value.bool_ = i;
+		break;
+	case JSON_FLOAT_VALUE:
+		e = malloc(sizeof *e);
+		if (!e)
+			goto out;
+		e->type = type;
+		d = va_arg(ap, double);
+		e->value.double_ = d;
+		break;
+	case JSON_STRING_VALUE:
+		s = va_arg(ap, char *);
+		e = json_str_new(s);
+		break;
+	case JSON_ATTR_VALUE:
+		name = va_arg(ap, json_entity_t);
+		value = va_arg(ap, json_entity_t);
+		e = json_attr_new(name, value);
+		break;
+	case JSON_LIST_VALUE:
+		e = json_list_new();
+		break;
+	case JSON_DICT_VALUE:
+		e = json_dict_new();
+		break;
+	case JSON_NULL_VALUE:
+		break;
+	default:
+		assert(0 == "Invalid entity type");
+	}
+ out:
+	va_end(ap);
+	return e;
+ err:
+	free(e);
+	goto out;
+}
+
+void json_attr_add(json_entity_t d, json_entity_t a)
+{
+	assert(d->type == JSON_DICT_VALUE);
+	assert(a->type == JSON_ATTR_VALUE);
+	json_str_t s = a->value.attr_->name->value.str_;
+	hent_init(&a->value.attr_->attr_ent, s->str, s->str_len);
+	htbl_ins(d->value.dict_->attr_table, &a->value.attr_->attr_ent);
+}
+
+static void json_list_free(json_list_t a)
+{
+	json_entity_t i;
+	assert(a->base.type == JSON_LIST_VALUE);
+	while (!TAILQ_EMPTY(&a->item_list)) {
+		i = TAILQ_FIRST(&a->item_list);
+		TAILQ_REMOVE(&a->item_list, i, item_entry);
+		json_entity_free(i);
+	}
+	free(a);
+}
+
+static void json_str_free(json_str_t s)
+{
+	assert(s->base.type == JSON_STRING_VALUE);
+	free(s->str);
+	free(s);
+}
+
+static void json_attr_free(json_attr_t a)
+{
+	assert(a->base.type == JSON_ATTR_VALUE);
+	json_entity_free(a->name);
+	json_entity_free(a->value);
+	free(a);
+}
+
+static void json_dict_free(json_dict_t d)
+{
+	json_attr_t i;
+	hent_t ent;
+	htbl_t t;
+
+	t = d->attr_table;
+	while (!htbl_empty(t)) {
+		ent = htbl_first(t);
+		htbl_del(t, ent);
+		i = container_of(ent, struct json_attr_s, attr_ent);
+		json_attr_free(i);
+	}
+	htbl_free(t);
+	free(d);
+}
+
+void json_entity_free(json_entity_t e)
+{
+	if (!e)
+		return;
+	switch (e->type) {
+	case JSON_INT_VALUE:
+		free(e);
+		break;
+	case JSON_BOOL_VALUE:
+		free(e);
+		break;
+	case JSON_FLOAT_VALUE:
+		free(e);
+		break;
+	case JSON_STRING_VALUE:
+		json_str_free(e->value.str_);
+		break;
+	case JSON_ATTR_VALUE:
+		json_attr_free(e->value.attr_);
+		break;
+	case JSON_LIST_VALUE:
+		json_list_free(e->value.list_);
+		break;
+	case JSON_DICT_VALUE:
+		json_dict_free(e->value.dict_);
+		break;
+	case JSON_NULL_VALUE:
+		free(e);
+		break;
+	default:
+		/* Leak if we're passed garbage */
+		return;
+	}
+}
+
+enum json_value_e json_entity_type(json_entity_t e)
+{
+	return e->type;
+}
+
+int64_t json_value_int(json_entity_t value)
+{
+	assert(value->type == JSON_INT_VALUE);
+	return value->value.int_;
+}
+
+int json_value_bool(json_entity_t value)
+{
+	assert(value->type == JSON_BOOL_VALUE);
+	return value->value.bool_;
+}
+
+double json_value_float(json_entity_t value)
+{
+	assert(value->type == JSON_FLOAT_VALUE);
+	return value->value.double_;
+}
+
+json_str_t json_value_str(json_entity_t value)
+{
+	assert(value->type == JSON_STRING_VALUE);
+	return value->value.str_;
+}
+
+json_attr_t json_value_attr(json_entity_t value)
+{
+	assert(value->type == JSON_ATTR_VALUE);
+	return value->value.attr_;
+}
+
+json_list_t json_value_list(json_entity_t value)
+{
+	assert(value->type == JSON_LIST_VALUE);
+	return value->value.list_;
+}
+
+json_dict_t json_value_dict(json_entity_t value)
+{
+	assert(value->type == JSON_DICT_VALUE);
+	return value->value.dict_;
+}
+
+json_str_t json_attr_name(json_entity_t attr)
+{
+	assert(attr->type == JSON_ATTR_VALUE);
+	return attr->value.attr_->name->value.str_;
+}
+
+json_entity_t json_attr_value(json_entity_t attr)
+{
+	assert(attr->type == JSON_ATTR_VALUE);
+	return attr->value.attr_->value;
+}
+
+size_t json_list_len(json_entity_t list)
+{
+	assert(list->type == JSON_LIST_VALUE);
+	return list->value.list_->item_count;
+}
---
+++ lib/src/json/json_util.h
@@ -0,0 +1,169 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2018 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2018 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef _JSON_UTIL_H_
+#define _JSON_UTIL_H_
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <coll/htbl.h>
+#include <stdarg.h>
+
+typedef struct json_str_s *json_str_t;
+typedef struct json_attr_s *json_attr_t;
+typedef struct json_list_s *json_list_t;
+typedef struct json_dict_s *json_dict_t;
+typedef struct json_entity_s *json_entity_t;
+
+enum json_value_e {
+	JSON_INT_VALUE,
+	JSON_BOOL_VALUE,
+	JSON_FLOAT_VALUE,
+	JSON_STRING_VALUE,
+	JSON_ATTR_VALUE,
+	JSON_LIST_VALUE,
+	JSON_DICT_VALUE,
+	JSON_NULL_VALUE
+};
+
+static const char *json_type_names[] = {
+	"INT", "BOOL", "FLOAT", "STRING", "ATTR", "LIST", "DICT", "NULL"
+};
+
+struct json_entity_s {
+	enum json_value_e type;
+	union {
+		int bool_;
+		int64_t int_;
+		double double_;
+		json_str_t str_;
+		json_attr_t attr_;
+		json_list_t list_;
+		json_dict_t dict_;
+	} value;
+	TAILQ_ENTRY(json_entity_s) item_entry;
+};
+
+struct json_str_s {
+	struct json_entity_s base;
+	char *str;
+	size_t str_len;
+};
+
+struct json_list_s {
+	struct json_entity_s base;
+	int item_count;
+	TAILQ_HEAD(json_item_list, json_entity_s) item_list;
+};
+
+struct json_attr_s {
+	struct json_entity_s base;
+	json_entity_t name;
+	json_entity_t value;
+	struct hent attr_ent;
+};
+
+struct json_dict_s {
+	struct json_entity_s base;
+	htbl_t attr_table;
+};
+
+struct json_loc_s {
+	int first_line;
+	int first_column;
+	int last_line;
+	int last_column;
+	char *filename;
+};
+
+typedef void *yyscan_t;
+typedef struct json_parser_s {
+	yyscan_t scanner;
+	struct yy_buffer_state *buffer_state;
+} *json_parser_t;
+
+extern json_parser_t json_parser_new(size_t user_data);
+extern void json_parser_free(json_parser_t p);
+extern void json_entity_free(json_entity_t e);
+extern enum json_value_e json_entity_type(json_entity_t e);
+extern json_entity_t json_attr_find(json_entity_t d, char *name);
+extern json_entity_t json_attr_first(json_entity_t d);
+extern json_entity_t json_attr_next(json_entity_t a);
+extern int json_parse_buffer(json_parser_t p, char *buf, size_t buf_len, json_entity_t *e);
+
+extern json_entity_t json_entity_new(enum json_value_e type, ...);
+
+extern size_t json_list_len(json_entity_t l);
+extern void json_item_add(json_entity_t a, json_entity_t e);
+extern void json_attr_add(json_entity_t d, json_entity_t a);
+extern json_entity_t json_item_first(json_entity_t a);
+extern json_entity_t json_item_next(json_entity_t i);
+extern json_str_t json_attr_name(json_entity_t a);
+extern json_entity_t json_attr_value(json_entity_t a);
+
+extern int64_t json_value_int(json_entity_t e);
+extern int json_value_bool(json_entity_t e);
+extern double json_value_float(json_entity_t e);
+extern json_str_t json_value_str(json_entity_t e);
+extern json_dict_t json_value_dict(json_entity_t e);
+extern json_list_t json_value_list(json_entity_t e);
+
+typedef struct jbuf_s {
+	size_t buf_len;
+	int cursor;
+	char buf[0];
+} *jbuf_t;
+
+extern jbuf_t jbuf_new(void);
+extern jbuf_t jbuf_append_attr(jbuf_t jb, const char *name, const char *fmt, ...);
+extern jbuf_t jbuf_append_str(jbuf_t jb, const char *fmt, ...);
+extern jbuf_t jbuf_append_va(jbuf_t jb, const char *fmt, va_list ap);
+extern void jbuf_free(jbuf_t jb);
+
+#define YYSTYPE json_entity_t
+#endif
