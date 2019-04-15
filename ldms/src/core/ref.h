1c35923cc11d4df48284d38cb31c305b4039adf9
Tom Tucker <tom@ogc.us>
2019-04-15 08:07:08 -0500

    Move ref to core

--- ldms/src/core/ref.h
+++ ldms/src/core/ref.h
@@ -2,7 +2,9 @@
 #define _REF_H_
 #include <sys/queue.h>
 #include <assert.h>
+#include <pthread.h>
 
+#ifdef _REF_TRACK_
 typedef struct ref_inst_s {
 	const char *get_func;
 	const char *put_func;
@@ -12,21 +14,25 @@ typedef struct ref_inst_s {
 	int ref_count;
 	LIST_ENTRY(ref_inst_s) entry;
 } *ref_inst_t;
+#endif
 
 typedef void (*ref_free_fn_t)(void *arg);
 typedef struct ref_s {
-	pthread_mutex_t lock;
+	int ref_count;		/* for all ref instances */
 	ref_free_fn_t free_fn;
 	void *free_arg;
+#ifdef _REF_TRACK_
+	pthread_mutex_t lock;
 	LIST_HEAD(, ref_inst_s) head;
-	int ref_count;		/* for all ref instances */
+#endif
 } *ref_t;
 
 static inline int _ref_put(ref_t r, const char *name, const char *func, int line)
 {
+	int count;
+#ifdef _REF_TRACK_
 	void ldmsd_lcritical(const char *fmt, ...);
 	ref_inst_t inst;
-	int count;
 	pthread_mutex_lock(&r->lock);
 	LIST_FOREACH(inst, &r->head, entry) {
 		if (0 == strcmp(inst->name, name)) {
@@ -49,12 +55,18 @@ static inline int _ref_put(ref_t r, const char *name, const char *func, int line
 	if (!count)
 		r->free_fn(r->free_arg);
 	pthread_mutex_unlock(&r->lock);
+#else
+	count = __sync_sub_and_fetch(&r->ref_count, 1);
+	if (!count)
+		r->free_fn(r->free_arg);
+#endif
 	return count;
 }
 #define ref_put(_r_, _n_) _ref_put((_r_), (_n_), __func__, __LINE__)
 
 static inline void _ref_get(ref_t r, const char *name, const char *func, int line)
 {
+#ifdef _REF_TRACK_
 	void ldmsd_lcritical(const char *fmt, ...);
 	ref_inst_t inst;
 	pthread_mutex_lock(&r->lock);
@@ -84,6 +96,9 @@ static inline void _ref_get(ref_t r, const char *name, const char *func, int lin
 	LIST_INSERT_HEAD(&r->head, inst, entry);
  out:
 	pthread_mutex_unlock(&r->lock);
+#else
+	__sync_fetch_and_add(&r->ref_count, 1);
+#endif
 }
 #define ref_get(_r_, _n_) _ref_get((_r_), (_n_), __func__, __LINE__)
 
@@ -91,18 +106,20 @@ static inline void _ref_init(ref_t r, const char *name,
 			     ref_free_fn_t fn, void *arg,
 			     const char *func, int line)
 {
+#ifdef _REF_TRACK_
 	ref_inst_t inst;
 	pthread_mutex_init(&r->lock, NULL);
-	r->free_fn = fn;
-	r->free_arg = arg;
 	LIST_INIT(&r->head);
 	inst = calloc(1, sizeof *inst); assert(inst);
 	inst->get_func = func;
 	inst->get_line = line;
 	inst->name = name;
 	inst->ref_count = 1;
-	r->ref_count = 1;
 	LIST_INSERT_HEAD(&r->head, inst, entry);
+#endif
+	r->free_fn = fn;
+	r->free_arg = arg;
+	r->ref_count = 1;
 }
 #define ref_init(_r_, _n_, _f_, _a_) _ref_init((_r_), (_n_), (_f_), (_a_), __func__, __LINE__)
 
