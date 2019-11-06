3a956ebd430b333990c77450ff296e93ca1fda2e
Narate Taerat <narate@ogc.us>
2019-11-06 17:03:00 -0600

    Add ldmsd_stream_publish and ldmsd_stream_subscribe test tools

---
+++ ldms/src/ldmsd/test/ldmsd_stream_publish.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <json/json_util.h>
+#include <ovis_util/util.h>
+#include "ldms.h"
+#include "../ldmsd_request.h"
+#include "../ldmsd_stream.h"
+
+static struct option long_opts[] = {
+	{"host",     required_argument, 0,  'h' },
+	{"port",     required_argument, 0,  'p' },
+	{"file",     required_argument, 0,  'f' },
+	{"stream",   required_argument, 0,  's' },
+	{"type",     required_argument, 0,  't' },
+	{"xprt",     required_argument, 0,  'x' },
+	{"auth",     required_argument, 0,  'a' },
+	{"auth_arg", required_argument, 0,  'A' },
+	{0,          0,                 0,  0 }
+};
+
+void usage(int argc, char **argv)
+{
+	printf("usage: %s -x <xprt> -h <host> -p <port> "
+	       "-s <stream-name> -t <stream-type> "
+	       "-f <file> -a <auth> -A <auth-opt>\n",
+	       argv[0]);
+	exit(1);
+}
+
+static const char *short_opts = "h:p:f:s:t:x:a:A:v";
+
+#define AUTH_OPT_MAX 128
+
+int main(int argc, char **argv)
+{
+	char *host = NULL;
+	char *port = NULL;
+	char *xprt = "sock";
+	char *filename = NULL;
+	char *stream = NULL;
+	int opt, opt_idx;
+	char *lval, *rval;
+	char *auth = "none";
+	struct attr_value_list *auth_opt = NULL;
+	const int auth_opt_max = AUTH_OPT_MAX;
+	FILE *file;
+	const char *stream_type = "string";
+
+	auth_opt = av_new(auth_opt_max);
+	if (!auth_opt) {
+		perror("could not allocate auth options");
+		exit(1);
+	}
+
+	while ((opt = getopt_long(argc, argv,
+				  short_opts, long_opts,
+				  &opt_idx)) > 0) {
+		switch (opt) {
+		case 'h':
+			host = strdup(optarg);
+			break;
+		case 'p':
+			port = strdup(optarg);
+			break;
+		case 'x':
+			xprt = strdup(optarg);
+			break;
+		case 'a':
+			auth = strdup(optarg);
+			break;
+		case 'A':
+			lval = strtok(optarg, "=");
+			rval = strtok(NULL, "");
+			if (!lval || !rval) {
+				printf("ERROR: Expecting -A name=value");
+				exit(1);
+			}
+			if (auth_opt->count == auth_opt->size) {
+				printf("ERROR: Too many auth options");
+				exit(1);
+			}
+			auth_opt->list[auth_opt->count].name = lval;
+			auth_opt->list[auth_opt->count].value = rval;
+			auth_opt->count++;
+			break;
+		case 's':
+			stream = strdup(optarg);
+			break;
+		case 'f':
+			filename = strdup(optarg);
+			break;
+		case 't':
+			if (0 == strcmp("json", optarg)) {
+				stream_type = "json";
+			} else if (0 == strcmp("string", optarg)) {
+				stream_type = "string";
+			} else {
+				printf("The type argument must be 'json' or 'string'\n");
+				usage(argc, argv);
+			}
+			break;
+		default:
+			usage(argc, argv);
+		}
+	}
+	if (!host || !port || !stream)
+		usage(argc, argv);
+
+	if (filename)
+		file = fopen(filename, "r");
+	else
+		file = stdin;
+	return ldmsd_stream_publish_file(stream, stream_type, xprt, host, port, auth, auth_opt, file);
+}
