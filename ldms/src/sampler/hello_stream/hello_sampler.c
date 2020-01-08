8c73116dcaf8aff67dfc43c0bb991b2f4d6b68f0
Tom Tucker <tom@ogc.us>
2020-01-08 14:55:35 -0600

    Implement a sample "Hello World" streams application and plugin.

---
+++ ldms/src/sampler/hello_stream/hello_sampler.c
@@ -0,0 +1,164 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2019 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2019 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#define _GNU_SOURCE
+#include <inttypes.h>
+#include <unistd.h>
+#include <sys/errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/inotify.h>
+#include <time.h>
+#include <pthread.h>
+#include <strings.h>
+#include <ctype.h>
+#include <pwd.h>
+#include <grp.h>
+#include <json/json_util.h>
+#include <assert.h>
+#include <sched.h>
+#include "ldms.h"
+#include "ldmsd.h"
+#include "ldmsd_stream.h"
+
+static ldmsd_msg_log_f msglog;
+static char *stream;
+
+static const char *usage(struct ldmsd_plugin *self)
+{
+	return  "config name=hello_sampler producer=<producer_name> instance=<instance_name>\n"
+		"         [stream=<stream_name>] [component_id=<component_id>] [perm=<permissions>]\n"
+                "         [uid=<user_name>] [gid=<group_name>] [job_count=<job_length>]\n"
+                "         [task_count=<task_length>]\n"
+		"     producer      A unique name for the host providing the data\n"
+		"     instance      A unique name for the metric set\n"
+		"     stream        A stream name to subscribe the hello sampler to.\n"
+		"                   Defaults to 'hello'\n";
+}
+
+static ldms_set_t get_set(struct ldmsd_sampler *self)
+{
+	return NULL;
+}
+
+static int sample(struct ldmsd_sampler *self)
+{
+	return 0;
+}
+
+static int hello_recv_cb(ldmsd_stream_client_t c, void *ctxt,
+			 ldmsd_stream_type_t stream_type,
+			 const char *msg, size_t msg_len,
+			 json_entity_t entity)
+{
+	int rc = 0;
+	const char *type;
+	switch (stream_type) {
+	case LDMSD_STREAM_JSON:
+		type = "JSON";
+		break;
+	case LDMSD_STREAM_STRING:
+		type = "STRING";
+		break;
+	}
+	msglog(LDMSD_LCRITICAL, "stream_type: %s, msg: \"%s\", msg_len: %d, entity: %p\n",
+	       type, msg, msg_len, entity);
+	return rc;
+}
+
+static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl,
+		  struct attr_value_list *avl)
+{
+	char *value;
+	int rc = 0;
+
+	value = av_value(avl, "stream");
+	if (value)
+		stream = strdup(value);
+	else
+		stream = strdup("hello_stream/hello");
+
+	ldmsd_stream_subscribe(stream, hello_recv_cb, self);
+
+	return rc;
+}
+
+static void term(struct ldmsd_plugin *self)
+{
+}
+
+static struct ldmsd_sampler hello_sampler = {
+	.base = {
+		.name = "hello_sampler",
+		.type = LDMSD_PLUGIN_SAMPLER,
+		.term = term,
+		.config = config,
+		.usage = usage,
+	},
+	.get_set = get_set,
+	.sample = sample
+};
+
+struct ldmsd_plugin *get_plugin(ldmsd_msg_log_f pf)
+{
+	msglog = pf;
+	return &hello_sampler.base;
+}
+
+static void __attribute__ ((constructor)) hello_sampler_init(void)
+{
+}
+
+static void __attribute__ ((destructor)) hello_sampler_term(void)
+{
+}
