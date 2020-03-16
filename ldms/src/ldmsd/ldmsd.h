83eb4389812dbb8ab5d4b88c8c8069b1521c9115
Narate Taerat <narate@ogc.us>
2020-03-16 05:41:58 -0500

    Remove left-over prdcr function residue in ldmsd.h

--- ldms/src/ldmsd/ldmsd.h
+++ ldms/src/ldmsd/ldmsd.h
@@ -886,8 +886,6 @@ static inline const char *ldmsd_prdcr_set_state_str(enum ldmsd_prdcr_set_state s
 	}
 	return "BAD STATE";
 }
-void ldmsd_prdcr_set_ref_get(ldmsd_prdcr_set_t set);
-void ldmsd_prdcr_set_ref_put(ldmsd_prdcr_set_t set);
 int ldmsd_prdcr_start(const char *name, const char *interval_str,
 		      ldmsd_sec_ctxt_t ctxt, int flags);
 int ldmsd_prdcr_start_regex(const char *prdcr_regex, const char *interval_str,
