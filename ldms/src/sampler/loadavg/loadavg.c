f4a9e48b79f966f07d7d0222a1f7551803bffdff
Benjamin Allan <baallan@sandia.gov>
2020-04-09 15:29:52 -0600

    Clean up bits from code review & make int coerce optional.
    Copyright notice question resolved.
    Also update make-all-top from v3 to v4, long overdue, for testing.

--- ldms/src/sampler/loadavg/loadavg.c
+++ ldms/src/sampler/loadavg/loadavg.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2020 National Technology & Engineering Solutions
  * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
  * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2011-2018 Open Grid Computing, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -48,6 +49,13 @@
 /**
  * \file loadavg.c
  * \brief /proc/loadavg data provider
+ *
+ *  This sampler presently shifts decimal load average values to an integer multiple because of function store limitations
+ *  rather than it being the right thing to do long term.
+ *
+ * Compile with
+ * -DLOADAVG_TYPE_DEBUG to enable type debugging output.
+ * -DLOADAVG_CONFIG_DEBUG to enable configuration debugging output.
  */
 #define _GNU_SOURCE
 #include <inttypes.h>
@@ -63,18 +71,17 @@
 #include "ldmsd.h"
 #include "sampler_base.h"
 
-
 #define PROC_FILE "/proc/loadavg"
 static char *procfile = PROC_FILE;
 
 static ldms_set_t set;
 
 #define SAMP "loadavg"
-static char *default_schema_name = SAMP;
 static FILE *mf;
 static ldmsd_msg_log_f msglog;
 static int metric_offset;
 static base_data_t base;
+static unsigned force_integer = 0;
 
 union la_value {
 	double d;
@@ -104,7 +111,7 @@ struct use_met metname[] = {
 
 #define METLEN 6
 
-#ifdef DUMP_LOADAVG
+#ifdef LOADAVG_CONFIG_DEBUG
 static void dump_metrics() {
 	int i;
 	for (i = 0; i < METLEN; i++) {
@@ -156,9 +163,13 @@ static const char *make_schema_name() {
 		if (metname[i].collect)
 			k += 1;
 	if (k == METLEN) {
-		return default_schema_name;
+		if (force_integer)
+			return SAMP "i";
+		else
+			return SAMP;
 	}
-	snprintf(der_schema, sizeof(der_schema), "loadavg%d%d%d%d%d%d",
+	snprintf(der_schema, sizeof(der_schema), "loadavg%s%d%d%d%d%d%d",
+		(force_integer ? "i" : ""),
 		metname[0].collect,
 		metname[1].collect,
 		metname[2].collect,
@@ -202,22 +213,28 @@ static int create_metric_set(base_data_t base)
 	/* Make sure these are added in the order they will appear in the file */
 	for (i = 0; i < METLEN; i++) {
 		if (metname[i].collect) {
-			/*
-			msglog(LDMSD_LDEBUG, SAMP ": adding metric %s %d\n",
-				metname[i].name, (int)metname[i].vtype);
-			*/
-			/* rc = ldms_schema_metric_add(schema, metname[i].name, metname[i].vtype); */
-			/* all coerced to u64 for now... */
-			rc = ldms_schema_metric_add(schema, metname[i].name, LDMS_V_U64);
+			if (force_integer) {
+				rc = ldms_schema_metric_add(schema, metname[i].name, LDMS_V_U64);
+#ifdef LOADAVG_TYPE_DEBUG
+				msglog(LDMSD_LDEBUG, SAMP ": adding metric %s LDMS_V_U64\n",
+					metname[i].name);
+#endif
+			} else {
+				rc = ldms_schema_metric_add(schema, metname[i].name, metname[i].vtype);
+#ifdef LOADAVG_TYPE_DEBUG
+				msglog(LDMSD_LDEBUG, SAMP ": adding metric %s %d\n",
+					metname[i].name, (int)metname[i].vtype);
+#endif
+			}
 			if (rc < 0) {
 				rc = ENOMEM;
 				goto err;
 			}
+#ifdef LOADAVG_TYPE_DEBUG
 		} else {
-			/*
 			msglog(LDMSD_LDEBUG, SAMP ": skipping metric %s\n",
 				metname[i].name);
-			*/
+#endif
 		}
 	}
 	set = base_set_new(base);
@@ -257,7 +274,9 @@ static int config_check(struct attr_value_list *kwl, struct attr_value_list *avl
 
 static const char *usage(struct ldmsd_plugin *self)
 {
-	return  "config name=" SAMP BASE_CONFIG_USAGE " [metrics=<mlist>]\n"
+	return  "config name=" SAMP BASE_CONFIG_USAGE " [metrics=<mlist>] [force_integer]\n"
+		"                 If force_integer is present, coerces load averages into (uint64_t)100*value\n"
+		"                 and changes the default schema name base from loadavg to loadavgi\n"
 		"    <sname>      The schema name, if generated default is not good enough.\n"
 		"    <mlist>      comma separated list of metrics to include. If not given, all are included.\n"
 		"                 complete list is " MET1 "," MET2 "," MET3 "," MET4 "," MET5 "," MET6 "\n"
@@ -267,12 +286,9 @@ static const char *usage(struct ldmsd_plugin *self)
 /**
  * \brief Configuration
  *
- * config name=loadavg producer_name=<name> instance_name=<instance_name> [component_id=<compid> schema=<sname>] [metrics=list]
- *     producer_name    The producer id value.
- *     instance_name    The set name.
- *     component_id     The component id. Defaults to zero
- *     sname            Optional schema name. Defaults to meminfo
- *     list		csv list of wanted metrics. defaults to all.
+ * config name=loadavg <sampler_ base options> [metrics=list]
+ *     list		csv list of wanted metrics. All metrics are included and schema is 'loadavg' if metrics is unspecified.
+ * The schema name will (if not supplied) default to a unique short name dependent on the list of metrics.
  */
 static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct attr_value_list *avl)
 {
@@ -288,12 +304,18 @@ static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct
 		return rc;
 	}
 
+	int force = av_idx_of(kwl, "force_integer");
+	if (force != -1) {
+		force_integer = 1;
+	}
+
 	char *metrics = av_value(avl, "metrics");
 	if (parse_metrics(metrics)) {
 		return EINVAL;
 	}
-#ifdef DUMP_LOADAVG
+#ifdef LOADAVG_CONFIG_DEBUG
 	dump_metrics();
+	msglog(LDMSD_LDEBUG, SAMP ": force_integer=%d.\n", force_integer);
 #endif
 	const char *def_schema_name = make_schema_name();
 
@@ -366,9 +388,6 @@ static int sample(struct ldmsd_sampler *self)
 	}
 	rc = sscanf(lbuf, LINE_FMT, &metname[0].v.d, &metname[1].v.d, &metname[2].v.d,
 		&metname[3].v.u, &metname[4].v.u, &metname[5].v.u);
-	/*
-	msglog(LDMSD_LDEBUG, SAMP ": %g %g %g %" PRIu64 "/%" PRIu64 " %" PRIu64 "\n", metname[0].v.d, metname[1].v.d, metname[2].v.d, metname[3].v.u, metname[4].v.u, metname[5].v.u);
-	*/
 	if (rc < METLEN) {
 		rc = EINVAL;
 		msglog(LDMSD_LERROR, SAMP ": fail " PROC_FILE "\n");
@@ -380,8 +399,12 @@ static int sample(struct ldmsd_sampler *self)
 		if (metname[i].collect) {
 			switch (metname[i].vtype) {
 			case LDMS_V_D64:
-				ldms_metric_set_u64(set, (j + metric_offset),
-					(uint64_t)(100*metname[i].v.d));
+				if (force_integer)
+					ldms_metric_set_u64(set, (j + metric_offset),
+						(uint64_t)(100*metname[i].v.d));
+				else
+					ldms_metric_set_double(set, (j + metric_offset),
+						metname[i].v.d);
 				break;
 			case LDMS_V_U64:
 				ldms_metric_set_u64(set, (j + metric_offset),
