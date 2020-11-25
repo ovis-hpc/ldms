d31dfd53cac655f45a0385da8cb73241fbfab201
Nichamon Naksinehaboon <nichamon@opengridcomputing.com>
2020-11-25 15:49:09 -0600

    Add a DEBUG code to track Zap endpoint
    The code is enabled by -D_ZAP_EP_TRACK_ at configure time.

--- lib/src/zap/zap.c
+++ lib/src/zap/zap.c
@@ -493,6 +493,11 @@ void zap_interpose_event(zap_ep_t ep, void *ctxt)
 static
 struct zap_event_queue *__get_least_busy_zap_event_queue();
 
+#ifdef _ZAP_EP_TRACK_
+TAILQ_HEAD(zap_ep_list, zap_ep) zap_ep_list;
+pthread_mutex_t zap_ep_list_lock;
+#endif /* _ZAP_EP_TRACK_ */
+
 zap_ep_t zap_new(zap_t z, zap_cb_fn_t cb)
 {
 	zap_ep_t zep = NULL;
@@ -510,6 +515,11 @@ zap_ep_t zap_new(zap_t z, zap_cb_fn_t cb)
 	pthread_mutex_init(&zep->lock, NULL);
 	sem_init(&zep->block_sem, 0, 0);
 	zep->event_queue = __get_least_busy_zap_event_queue();
+#ifdef _ZAP_EP_TRACK_
+	pthread_mutex_lock(&zap_ep_list_lock);
+	TAILQ_INSERT_TAIL(&zap_ep_list, zep, ep_link);
+	pthread_mutex_unlock(&zap_ep_list_lock);
+#endif /* _ZAP_EP_TRACK_ */
 	return zep;
 }
 
@@ -642,6 +652,11 @@ void zap_put_ep(zap_ep_t ep)
 	assert(ep->ref_count);
 	if (0 == __sync_sub_and_fetch(&ep->ref_count, 1)) {
 		zap_event_queue_ep_put(ep->event_queue);
+#ifdef _ZAP_EP_TRACK_
+		pthread_mutex_lock(&zap_ep_list_lock);
+		TAILQ_REMOVE(&zap_ep_list, ep, ep_link);
+		pthread_mutex_unlock(&zap_ep_list_lock);
+#endif /* _ZAP_EP_TRACK_ */
 		ep->z->destroy(ep);
 	}
 }
@@ -1091,6 +1106,11 @@ static void zap_init(void)
 	}
 
 	pthread_mutex_init(&zap_list_lock, 0);
+
+#ifdef _ZAP_EP_TRACK_
+	TAILQ_INIT(&zap_ep_list);
+	pthread_mutex_init(&zap_ep_list_lock, 0);
+#endif /* _ZAP_EP_TRACK_ */
 
 	stats_w = ZAP_ENV_INT(ZAP_THRSTAT_WINDOW);
 	zap_event_workers = ZAP_ENV_INT(ZAP_EVENT_WORKERS);
