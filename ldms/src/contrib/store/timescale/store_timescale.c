0203beed2e547554ecfa50e4198e16596ddf852b
Tom Tucker <tom@ogc.us>
2020-12-27 12:36:00 -0700

    Revert "Update store_timescale.c"
    This reverts commit d16bf70e31369100aac03d6d7d2bd9599ed101cd.

--- ldms/src/contrib/store/timescale/store_timescale.c
+++ ldms/src/contrib/store/timescale/store_timescale.c
@@ -1,47 +1,8 @@
 /* -*- c-basic-offset: 8 -*-
  * Copyright (c) 2012-2018 Open Grid Computing, Inc. All rights reserved.
  * Copyright (c) 2020 HPC Center at Shanghai Jiao Tong University. All rights reserved.
- * 
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the BSD-type
- * license below:
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *      Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *
- *      Redistributions in binary form must reproduce the above
- *      copyright notice, this list of conditions and the following
- *      disclaimer in the documentation and/or other materials provided
- *      with the distribution.
- *
- *      Neither the name of Sandia nor the names of any contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
- *
- *      Neither the name of Open Grid Computing nor the names of any
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
- *
- *      Modified source versions must be plainly marked as such, and
- *      must not be misrepresented as being the original software.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ * Confidential and proprietary
  */
 
 #include <sys/queue.h>
@@ -211,15 +172,15 @@ static char *fixup(char *name)
  *   */
 static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct attr_value_list *avl)
 {
-        char *value, *pwfile = NULL;
-        pthread_mutex_lock(&cfg_lock);
+	char *value, *pwfile = NULL;
+	pthread_mutex_lock(&cfg_lock);
 
-        value = av_value(avl, "user");
-        if (!value) {
-                msglog(LDMSD_LERROR, "The 'user' keyword is required.\n");
-                return EINVAL;
-        }
-        strncpy(user, value, sizeof(user));
+	value = av_value(avl, "user");
+	if (!value) {
+		msglog(LDMSD_LERROR, "The 'user' keyword is required.\n");
+		return EINVAL;
+	}
+	strncpy(user, value, sizeof(user));
 
         pwfile = av_value(avl, "pwfile");
         if (!pwfile) {
@@ -257,7 +218,10 @@ static int config(struct ldmsd_plugin *self, struct attr_value_list *kwl, struct
                         return EINVAL;
                 }
                 strncpy(password, strdup(s), sizeof(password));
-
+//                if (!password) {
+//                        msglog(LDMSD_LERROR, "Auth error: Out of memory when trying to read the secret word.\n");
+//                        return EINVAL;
+//                }
                 fclose(file);
         }
 
@@ -304,9 +268,7 @@ static void term(struct ldmsd_plugin *self)
 
 static const char *usage(struct ldmsd_plugin *self)
 {
-	return "config name=store_timescale user=<username> pwfile=<full path to password file> "
-               "hostaddr=<host ip addr> port=<port no> dbname=<database name> "
-               "measurement_limit=<sql statement length>";
+	return "config name=store_timescale user=<username> pwfile=<full path to password file> hostaddr=<host ip addr> port=<port no> dbname=<database name> measurement_limit=<sql statement length>";
 }
 
 static ldmsd_store_handle_t
