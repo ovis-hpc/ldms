8e2eee54ea0ba43f10cf51aec7cc6f76f83fc93d
Christopher J. Morrone <morrone2@llnl.gov>
2021-03-15 14:24:13 -0700

    Introduce lustre_mdt sampler plugin
    This is the upstreamed version of the lustre_mdt sampler plugin from
    the ldms-plugins-llnl project version 1.14.

---
+++ ldms/src/sampler/lustre_mdt/lustre_mdt.h
@@ -0,0 +1,17 @@
+/* -*- c-basic-offset: 8 -*- */
+/* Copyright 2021 Lawrence Livermore National Security, LLC
+ * See the top-level COPYING file for details.
+ *
+ * SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+ */
+#ifndef __LUSTRE_MDT_H
+#define __LUSTRE_MDT_H
+
+#include "ldms.h"
+#include "ldmsd.h"
+
+#define SAMP "lustre_mdt"
+
+extern ldmsd_msg_log_f log_fn;
+
+#endif /* __LUSTRE_MDT_H */
