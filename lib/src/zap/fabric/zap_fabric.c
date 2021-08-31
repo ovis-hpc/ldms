nichamon/ovis

Add the zap_send_mapped() API to the zap library
Currently the zap_send() API receives application buffer and copies it
to another buffer that has been registered with the hardware using the
zap_map() interface. The zap_send() API is simple to use but is
inefficient when the same buffer is to be sent to many clients because
the same buffer is mapped and copied many times.

The zap_send_mapped() API has the following signature.

   int zap_send_mapped(zap_ep_t ep, zap_map_t map, void *buf,
                       size_t len, void *context)

The zap_send_mapped() API posts the RDMA_SEND operation directly from
the previously mapped buffer (map) without requiring registration or
copy by the transport. This significantly improves performance for
sending operation. Unlike the zap_send() API, the zap_send_mapped() API
delivers a completion event via the application callback function unless
the function return an error.

lib/src/zap/fabric/zap_fabric.c
@@ -1,8 +1,8 @@
 /* -*- c-basic-offset: 8 -*-
- * Copyright (c) 2013-2019 National Technology & Engineering Solutions
+ * Copyright (c) 2013-2020 National Technology & Engineering Solutions
  * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
  * NTESS, the U.S. Government retains certain rights in this software.
- * Copyright (c) 2013-2019 Open Grid Computing, Inc. All rights reserved.
+ * Copyright (c) 2013-2020 Open Grid Computing, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -99,7 +99,7 @@ static struct {
  *    For surfacing endpoint errors. Calls the endpoint's logging fn specified
  *    in zap_new.
  */
-#ifdef DEBUG
+#if defined(DEBUG) || defined(EP_DEBUG)
 static void dlog_(const char *func, int line, char *fmt, ...)
 {
 	va_list ap;
@@ -111,6 +111,7 @@ static void dlog_(const char *func, int line, char *fmt, ...)
 	clock_gettime(CLOCK_REALTIME, &ts);
 	va_start(ap, fmt);
 	vasprintf(&buf, fmt, ap);
+	va_end(ap);
 	g.log_fn("[%d] %d.%09d: %s:%d | %s", tid, ts.tv_sec, ts.tv_nsec, func, line, buf);
 	free(buf);
 }
@@ -120,7 +121,7 @@ static void dlog_(const char *func, int line, char *fmt, ...)
 		rep->ep.z->log_fn(fmt, ##__VA_ARGS__); \
 } while (0)
 
-#ifdef DEBUG
+#if defined(DEBUG) || defined(EP_DEBUG)
 #define DLOG(fmt, ...)	dlog_(__func__, __LINE__, fmt, ##__VA_ARGS__)
 #else
 #define DLOG(fmt, ...)
@@ -715,6 +716,10 @@ static void flush_io_q(struct z_fi_ep *rep)
 			ev.type = ZAP_EVENT_READ_COMPLETE;
 			ev.context = ctxt->usr_context;
 			break;
+		    case ZAP_WC_SEND_MAPPED:
+			ev.type = ZAP_EVENT_SEND_COMPLETE;
+			ev.context = ctxt->usr_context;
+			break;
 		    case ZAP_WC_RECV:
 		    default:
 			LOG_(rep, "invalid op type %d in queued i/o\n", ctxt->op);
@@ -746,6 +751,23 @@ post_wr(struct z_fi_ep *rep, struct z_fi_context *ctxt)
 
 		DLOG("ZAP_WC_SEND rep %p ctxt %p rb %p len %d with %d credits rc %d\n",
 		     rep, ctxt, rb, len, rep->lcl_rq_credits, rc);
+		break;
+	    case ZAP_WC_SEND_MAPPED:
+		rb = ctxt->u.send_mapped.rb;
+		rb->msg->credits = htons(rep->lcl_rq_credits);
+		rep->lcl_rq_credits = 0;
+		SEND_LOG(rep, ctxt);
+		/* NOTE: iov[0] contains prov_prefix and z_fi msg hdr using
+		 *              rb->buf,
+		 *       iov[1] contains application payload. */
+		rc = fi_sendv(rep->fi_ep, ctxt->u.send_mapped.iov,
+			      ctxt->u.send_mapped.mr_desc, 2, 0, ctxt);
+
+		DLOG("ZAP_WC_SEND_MAPPED rep %p ctxt %p rb %p hdr_len %d "
+				"payload_len %d with %d credits rc %d\n",
+		     rep, ctxt, rb, ctxt->u.send_mapped.iov[0].iov_len,
+		     ctxt->u.send_mapped.iov[1].iov_len, rep->lcl_rq_credits,
+		     rc);
 		break;
 	    case ZAP_WC_RDMA_WRITE:
 		rc = fi_write(rep->fi_ep, ctxt->u.rdma.src_addr, ctxt->u.rdma.len,
@@ -1029,7 +1051,8 @@ zap_err_t z_map_err(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
 	if (!entry->err)
 		return ZAP_ERR_OK;
 
-	if (0 == strcmp(rep->fi->fabric_attr->prov_name, "verbs"))
+	if (rep->fi->fabric_attr->prov_name &&
+			0 == strcmp(rep->fi->fabric_attr->prov_name, "verbs"))
 		goto verbs;
 
 	return zap_errno2zerr(entry->err);
@@ -1087,6 +1110,20 @@ static void process_send_wc(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
 	struct z_fi_context *ctxt = entry->op_context;
 	if (!entry->err && ctxt->u.send.rb)
 		__buffer_free(ctxt->u.send.rb);
+}
+
+static void process_send_mapped_wc(struct z_fi_ep *rep,
+				   struct fi_cq_err_entry *entry)
+{
+	struct z_fi_context *ctxt = entry->op_context;
+	struct zap_event zev = {
+			.type = ZAP_EVENT_SEND_COMPLETE,
+			.context = ctxt->usr_context,
+			.status = z_map_err(rep, entry),
+		};
+	rep->ep.cb(&rep->ep, &zev);
+	if (!entry->err && ctxt->u.send_mapped.rb)
+		__buffer_free(ctxt->u.send_mapped.rb);
 }
 
 static void process_read_wc(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
@@ -1367,6 +1404,14 @@ static void scrub_cq(struct z_fi_ep *rep)
 			DLOG("got ZAP_WC_SEND rep %p ctxt %p rb %p err %d proverr %d\n",
 			     rep, ctxt, ctxt->u.send.rb, entry.err, entry.prov_errno);
 			process_send_wc(rep, &entry);
+			put_sq(rep);
+			if (entry.err && ctxt->u.send.rb)
+				__buffer_free(ctxt->u.send.rb);
+			break;
+		    case ZAP_WC_SEND_MAPPED:
+			DLOG("got ZAP_WC_SEND rep %p ctxt %p rb %p err %d proverr %d\n",
+			     rep, ctxt, ctxt->u.send_mapped.rb, entry.err, entry.prov_errno);
+			process_send_mapped_wc(rep, &entry);
 			put_sq(rep);
 			if (entry.err && ctxt->u.send.rb)
 				__buffer_free(ctxt->u.send.rb);
@@ -1702,7 +1747,6 @@ static void handle_disconnected(struct z_fi_ep *rep, struct fi_eq_cm_entry *entr
 	pthread_mutex_unlock(&rep->credit_lock);
 
 	pthread_mutex_lock(&rep->ep.lock);
-
 	if (!LIST_EMPTY(&rep->active_ctxt_list)) {
 		rep->deferred_disconnected = 1;
 		pthread_mutex_unlock(&rep->ep.lock);
@@ -2104,6 +2148,62 @@ static zap_err_t z_fi_send(zap_ep_t ep, char *buf, size_t len)
 	return rc;
 }
 
+static zap_err_t z_fi_send_mapped(zap_ep_t ep, zap_map_t map,
+				  void *buf, size_t len, void *context)
+{
+	int rc, hdr_len;
+	struct z_fi_ep *rep = (struct z_fi_ep *)ep;
+	struct z_fi_context *ctxt;
+	struct z_fi_buffer *rbuf;
+	struct z_fi_map *rmap = (struct z_fi_map *)map;
+
+	if (map->type != ZAP_MAP_LOCAL)
+		return ZAP_ERR_INVALID_MAP_TYPE;
+	if (z_map_access_validate(map, buf, len, 0))
+		return ZAP_ERR_LOCAL_LEN;
+
+	pthread_mutex_lock(&rep->ep.lock);
+	rc = __ep_state_check(rep);
+	if (rc)
+		goto out;
+	rbuf = __buffer_alloc(rep);
+	if (!rbuf) {
+		rc = ZAP_ERR_RESOURCE;
+		goto out;
+	}
+	if (!_buffer_fits(rbuf, len+sizeof(struct z_fi_message_hdr))) {
+		rc = ZAP_ERR_LOCAL_LEN;
+		__buffer_free(rbuf);
+		goto out;
+	}
+	ctxt = __context_alloc(rep, context, ZAP_WC_SEND_MAPPED);
+	if (!ctxt) {
+		__buffer_free(rbuf);
+		rc = ZAP_ERR_RESOURCE;
+		goto out;
+	}
+	/* prefix + hdr */
+	hdr_len = rep->fi->ep_attr->msg_prefix_size + sizeof(struct z_fi_message_hdr);
+	rbuf->msg->msg_type = htons(Z_FI_MSG_SEND);
+	ctxt->u.send_mapped.rb = rbuf;
+	ctxt->u.send_mapped.iov[0].iov_base = rbuf->buf;
+	ctxt->u.send_mapped.iov[0].iov_len = hdr_len;
+	ctxt->u.send_mapped.mr_desc[0] = fi_mr_desc(rep->buf_pool_mr);
+	/* payload */
+	ctxt->u.send_mapped.iov[1].iov_base = buf;
+	ctxt->u.send_mapped.iov[1].iov_len = len;
+	ctxt->u.send_mapped.mr_desc[1] = fi_mr_desc(rmap->u.local.mr);
+
+	rc = submit_wr(rep, ctxt, 0);
+	if (rc) {
+		__context_free(ctxt);
+		__buffer_free(rbuf);
+	}
+out:
+	pthread_mutex_unlock(&rep->ep.lock);
+	return rc;
+}
+
 static zap_err_t z_fi_share(zap_ep_t ep, zap_map_t map,
 				const char *msg, size_t msg_len)
 {
@@ -2275,6 +2375,7 @@ out:
 
 static pthread_t cm_thread, cq_thread;
 
+__attribute__((unused))
 static void z_fi_cleanup(void)
 {
 	void *dontcare;
@@ -2321,7 +2422,7 @@ static int init_once()
 		z_fi_info_log_on = atoi(env);
 
 	init_complete = 1;
-	atexit(z_fi_cleanup);
+//	atexit(z_fi_cleanup);
 	return 0;
 
  err_3:
@@ -2338,6 +2439,9 @@ zap_err_t zap_transport_get(zap_t *pz, zap_log_fn_t log_fn,
 			    zap_mem_info_fn_t mem_info_fn)
 {
 	zap_t z;
+
+	if (!g.log_fn && log_fn)
+		g.log_fn = log_fn;
 
 	if (init_once())
 		goto err_0;
@@ -2356,15 +2460,13 @@ zap_err_t zap_transport_get(zap_t *pz, zap_log_fn_t log_fn,
 	z->listen = z_fi_listen;
 	z->close = z_fi_close;
 	z->send = z_fi_send;
+	z->send_mapped = z_fi_send_mapped;
 	z->read = z_fi_read;
 	z->write = z_fi_write;
 	z->map = z_fi_map;
 	z->unmap = z_fi_unmap;
 	z->share = z_fi_share;
 	z->get_name = z_get_name;
-
-	if (!g.log_fn && log_fn)
-		g.log_fn = log_fn;
 
 	*pz = z;
 	return ZAP_ERR_OK;

ovis-hpc/ldms

lib/src/zap/fabric/zap_fabric.c
@@ -1,8 +1,8 @@
 /* -*- c-basic-offset: 8 -*-
- * Copyright (c) 2013-2019 National Technology & Engineering Solutions
+ * Copyright (c) 2013-2020 National Technology & Engineering Solutions
  * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
  * NTESS, the U.S. Government retains certain rights in this software.
- * Copyright (c) 2013-2019 Open Grid Computing, Inc. All rights reserved.
+ * Copyright (c) 2013-2020 Open Grid Computing, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -107,7 +107,7 @@ static const char *z_fi_op_str[] = {
  *    For surfacing endpoint errors. Calls the endpoint's logging fn specified
  *    in zap_new.
  */
-#ifdef DEBUG
+#if defined(DEBUG) || defined(EP_DEBUG)
 static void dlog_(const char *func, int line, char *fmt, ...)
 {
 	va_list ap;
@@ -119,6 +119,7 @@ static void dlog_(const char *func, int line, char *fmt, ...)
 	clock_gettime(CLOCK_REALTIME, &ts);
 	va_start(ap, fmt);
 	vasprintf(&buf, fmt, ap);
+	va_end(ap);
 	g.log_fn("[%d] %d.%09d: %s:%d | %s", tid, ts.tv_sec, ts.tv_nsec, func, line, buf);
 	free(buf);
 }
@@ -128,7 +129,7 @@ static void dlog_(const char *func, int line, char *fmt, ...)
 		rep->ep.z->log_fn(fmt, ##__VA_ARGS__); \
 } while (0)
 
-#ifdef DEBUG
+#if defined(DEBUG) || defined(EP_DEBUG)
 #define DLOG(fmt, ...)	dlog_(__func__, __LINE__, fmt, ##__VA_ARGS__)
 #else
 #define DLOG(fmt, ...)
@@ -726,6 +727,10 @@ static void flush_io_q(struct z_fi_ep *rep)
 			ev.type = ZAP_EVENT_READ_COMPLETE;
 			ev.context = ctxt->usr_context;
 			break;
+		    case ZAP_WC_SEND_MAPPED:
+			ev.type = ZAP_EVENT_SEND_MAPPED_COMPLETE;
+			ev.context = ctxt->usr_context;
+			break;
 		    case ZAP_WC_RECV:
 		    default:
 			LOG_(rep, "invalid op type %d in queued i/o\n", ctxt->op);
@@ -758,6 +763,23 @@ post_wr(struct z_fi_ep *rep, struct z_fi_context *ctxt)
 
 		DLOG("ZAP_WC_SEND rep %p ctxt %p rb %p len %d with %d credits rc %d\n",
 		     rep, ctxt, rb, len, rep->lcl_rq_credits, rc);
+		break;
+	    case ZAP_WC_SEND_MAPPED:
+		rb = ctxt->u.send_mapped.rb;
+		rb->msg->credits = htons(rep->lcl_rq_credits);
+		rep->lcl_rq_credits = 0;
+		SEND_LOG(rep, ctxt);
+		/* NOTE: iov[0] contains prov_prefix and z_fi msg hdr using
+		 *              rb->buf,
+		 *       iov[1] contains application payload. */
+		rc = fi_sendv(rep->fi_ep, ctxt->u.send_mapped.iov,
+			      ctxt->u.send_mapped.mr_desc, 2, 0, ctxt);
+
+		DLOG("ZAP_WC_SEND_MAPPED rep %p ctxt %p rb %p hdr_len %d "
+				"payload_len %d with %d credits rc %d\n",
+		     rep, ctxt, rb, ctxt->u.send_mapped.iov[0].iov_len,
+		     ctxt->u.send_mapped.iov[1].iov_len, rep->lcl_rq_credits,
+		     rc);
 		break;
 	    case ZAP_WC_RDMA_WRITE:
 		rc = fi_write(rep->fi_ep, ctxt->u.rdma.src_addr, ctxt->u.rdma.len,
@@ -1053,7 +1075,8 @@ zap_err_t z_map_err(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
 	if (!entry->err)
 		return ZAP_ERR_OK;
 
-	if (0 == strcmp(rep->fi->fabric_attr->prov_name, "verbs"))
+	if (rep->fi->fabric_attr->prov_name &&
+			0 == strcmp(rep->fi->fabric_attr->prov_name, "verbs"))
 		goto verbs;
 
 	return zap_errno2zerr(entry->err);
@@ -1111,6 +1134,20 @@ static void process_send_wc(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
 	struct z_fi_context *ctxt = entry->op_context;
 	if (!entry->err && ctxt->u.send.rb)
 		__buffer_free(ctxt->u.send.rb);
+}
+
+static void process_send_mapped_wc(struct z_fi_ep *rep,
+				   struct fi_cq_err_entry *entry)
+{
+	struct z_fi_context *ctxt = entry->op_context;
+	struct zap_event zev = {
+			.type = ZAP_EVENT_SEND_MAPPED_COMPLETE,
+			.context = ctxt->usr_context,
+			.status = z_map_err(rep, entry),
+		};
+	rep->ep.cb(&rep->ep, &zev);
+	if (!entry->err && ctxt->u.send_mapped.rb)
+		__buffer_free(ctxt->u.send_mapped.rb);
 }
 
 static void process_read_wc(struct z_fi_ep *rep, struct fi_cq_err_entry *entry)
@@ -1398,6 +1435,14 @@ static void scrub_cq(struct z_fi_ep *rep)
 			DLOG("got ZAP_WC_SEND rep %p ctxt %p rb %p err %d proverr %d\n",
 			     rep, ctxt, ctxt->u.send.rb, entry.err, entry.prov_errno);
 			process_send_wc(rep, &entry);
+			put_sq(rep);
+			if (entry.err && ctxt->u.send.rb)
+				__buffer_free(ctxt->u.send.rb);
+			break;
+		    case ZAP_WC_SEND_MAPPED:
+			DLOG("got ZAP_WC_SEND rep %p ctxt %p rb %p err %d proverr %d\n",
+			     rep, ctxt, ctxt->u.send_mapped.rb, entry.err, entry.prov_errno);
+			process_send_mapped_wc(rep, &entry);
 			put_sq(rep);
 			if (entry.err && ctxt->u.send.rb)
 				__buffer_free(ctxt->u.send.rb);
@@ -1734,7 +1779,6 @@ static void handle_disconnected(struct z_fi_ep *rep, struct fi_eq_cm_entry *entr
 	pthread_mutex_unlock(&rep->credit_lock);
 
 	pthread_mutex_lock(&rep->ep.lock);
-
 	if (!LIST_EMPTY(&rep->active_ctxt_list)) {
 		rep->deferred_disconnected = 1;
 		pthread_mutex_unlock(&rep->ep.lock);
@@ -2142,6 +2186,62 @@ static zap_err_t z_fi_send(zap_ep_t ep, char *buf, size_t len)
 	return rc;
 }
 
+static zap_err_t z_fi_send_mapped(zap_ep_t ep, zap_map_t map,
+				  void *buf, size_t len, void *context)
+{
+	int rc, hdr_len;
+	struct z_fi_ep *rep = (struct z_fi_ep *)ep;
+	struct z_fi_context *ctxt;
+	struct z_fi_buffer *rbuf;
+	struct z_fi_map *rmap = (struct z_fi_map *)map;
+
+	if (map->type != ZAP_MAP_LOCAL)
+		return ZAP_ERR_INVALID_MAP_TYPE;
+	if (z_map_access_validate(map, buf, len, 0))
+		return ZAP_ERR_LOCAL_LEN;
+
+	pthread_mutex_lock(&rep->ep.lock);
+	rc = __ep_state_check(rep);
+	if (rc)
+		goto out;
+	rbuf = __buffer_alloc(rep);
+	if (!rbuf) {
+		rc = ZAP_ERR_RESOURCE;
+		goto out;
+	}
+	if (!_buffer_fits(rbuf, len+sizeof(struct z_fi_message_hdr))) {
+		rc = ZAP_ERR_LOCAL_LEN;
+		__buffer_free(rbuf);
+		goto out;
+	}
+	ctxt = __context_alloc(rep, context, ZAP_WC_SEND_MAPPED);
+	if (!ctxt) {
+		__buffer_free(rbuf);
+		rc = ZAP_ERR_RESOURCE;
+		goto out;
+	}
+	/* prefix + hdr */
+	hdr_len = rep->fi->ep_attr->msg_prefix_size + sizeof(struct z_fi_message_hdr);
+	rbuf->msg->msg_type = htons(Z_FI_MSG_SEND);
+	ctxt->u.send_mapped.rb = rbuf;
+	ctxt->u.send_mapped.iov[0].iov_base = rbuf->buf;
+	ctxt->u.send_mapped.iov[0].iov_len = hdr_len;
+	ctxt->u.send_mapped.mr_desc[0] = fi_mr_desc(rep->buf_pool_mr);
+	/* payload */
+	ctxt->u.send_mapped.iov[1].iov_base = buf;
+	ctxt->u.send_mapped.iov[1].iov_len = len;
+	ctxt->u.send_mapped.mr_desc[1] = fi_mr_desc(rmap->u.local.mr);
+
+	rc = submit_wr(rep, ctxt, 0);
+	if (rc) {
+		__context_free(ctxt);
+		__buffer_free(rbuf);
+	}
+out:
+	pthread_mutex_unlock(&rep->ep.lock);
+	return rc;
+}
+
 static zap_err_t z_fi_share(zap_ep_t ep, zap_map_t map,
 				const char *msg, size_t msg_len)
 {
@@ -2313,6 +2413,7 @@ out:
 
 static pthread_t cm_thread, cq_thread;
 
+__attribute__((unused))
 static void z_fi_cleanup(void)
 {
 	void *dontcare;
@@ -2359,7 +2460,7 @@ static int init_once()
 		z_fi_info_log_on = atoi(env);
 
 	init_complete = 1;
-	atexit(z_fi_cleanup);
+//	atexit(z_fi_cleanup);
 	return 0;
 
  err_3:
@@ -2397,6 +2498,7 @@ zap_err_t zap_transport_get(zap_t *pz, zap_log_fn_t log_fn,
 	z->listen = z_fi_listen;
 	z->close = z_fi_close;
 	z->send = z_fi_send;
+	z->send_mapped = z_fi_send_mapped;
 	z->read = z_fi_read;
 	z->write = z_fi_write;
 	z->map = z_fi_map;