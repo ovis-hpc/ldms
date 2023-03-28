04e84a52ff824c72080232e79d2e4bfd9707ded1
Christopher J. Morrone <morrone2@llnl.gov>
2023-03-28 16:43:29 -0700

    Retire the genders configuration method.

--- util/sample_init_scripts/genders/systemd/etc/sysconfig/ldms.d/plugins-conf/node_rate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "trnsfp_add name=meminfo-rate plugin=trnsf_rate mode=published input_schema=meminfo output_schema=meminfo-rate test=1"
-echo "trnsfp_start name=meminfo-rate"
-echo "trnsfp_add name=vmstat-rate plugin=trnsf_rate mode=published input_schema=vmstat output_schema=vmstat-rate test=1"
-echo "trnsfp_start name=vmstat-rate"
-echo "trnsfp_add name=msr_interlagos-rate plugin=trnsf_rate mode=published input_schema=msr_interlagos_delta_sum_vector metrics=Ctr4_c,Ctr5_c test=1"
-echo "trnsfp_start name=msr_interlagos-rate"
-
