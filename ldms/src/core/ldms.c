nichamon/ovis

Get rid of the log function handle in ldms
Without the change, LDMS receives the application's log function handle at
transport creation. LDMS cannot access the log function handle when it performs
set and schema manipulations. The approach also limits LDMS logging messages of
a single severity level. The debugging messages can be turned on/off at the
compile time or via a gdb session, which is inconvenient.
The change refactors LDMS to use libovis_log facilities and removes the logic
that passes around a log function handle. The change does not add log messages.

ldms/src/core/ldms.c
@@ -73,6 +73,8 @@
 #include "ldms_private.h"
 #include "coll/rbt.h"
 
+ovis_log_t xlog;
+
 #define SET_DIR_PATH "/var/run/ldms"
 static char *__set_dir = SET_DIR_PATH;
 #define SET_DIR_LEN sizeof(SET_DIR_PATH)
@@ -1076,6 +1078,13 @@ int delete_thread_init_once()
 int ldms_init(size_t max_size)
 {
 	size_t grain = LDMS_GRAIN_MMALLOC;
+
+	xlog = ovis_log_create("xprt.ldms", "Messages for ldms");
+	if (!xlog) {
+		ovis_log(NULL, OVIS_LWARN, "Failed to create ldms's "
+				"log subsystem. Error %d.\n", errno);
+	}
+
 	int rc = mm_init(max_size, grain); /* mm_init() returns errno */
 	if (rc)
 		return rc;

ovis-hpc/ldms

ldms/src/core/ldms.c
@@ -73,6 +73,8 @@
 #include "ldms_private.h"
 #include "coll/rbt.h"
 
+ovis_log_t xlog;
+
 #define SET_DIR_PATH "/var/run/ldms"
 static char *__set_dir = SET_DIR_PATH;
 #define SET_DIR_LEN sizeof(SET_DIR_PATH)
@@ -1083,6 +1085,13 @@ int delete_thread_init_once()
 int ldms_init(size_t max_size)
 {
 	size_t grain = LDMS_GRAIN_MMALLOC;
+
+	xlog = ovis_log_register("xprt.ldms", "Messages for ldms");
+	if (!xlog) {
+		ovis_log(NULL, OVIS_LWARN, "Failed to create ldms's "
+				"log subsystem. Error %d.\n", errno);
+	}
+
 	int rc = mm_init(max_size, grain); /* mm_init() returns errno */
 	if (rc)
 		return rc;