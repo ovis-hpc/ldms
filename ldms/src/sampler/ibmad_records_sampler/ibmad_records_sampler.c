b0d35f50c8dfbbbe1dab8c90e3add52a172d9305
Narate Taerat <narate@ogc.us>
2023-08-10 15:22:18 -0500

    Fix print format in ibmad_sampler and ibmad_records_sampler

--- ldms/src/sampler/ibmad_records_sampler/ibmad_records_sampler.c
+++ ldms/src/sampler/ibmad_records_sampler/ibmad_records_sampler.c
@@ -667,7 +667,7 @@ static void dump_port_filters()
 	for (i = 0 ; i < MAX_CA_NAMES; i++) {
 		if (conf.ports[i].ca_name[0] == '\0')
 			break;
-		ovis_log(mylog, OVIS_LDEBUG, SAMP ": dpf: %s : 0x%x\n",
+		ovis_log(mylog, OVIS_LDEBUG, SAMP ": dpf: %s : 0x%lx\n",
 			conf.ports[i].ca_name, conf.ports[i].port_bits);
 	}
 }
