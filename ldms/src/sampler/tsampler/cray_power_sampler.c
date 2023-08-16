273bcdc99c43dba0270f73f653e2f9dc6201fa1f
Ben Allan <baallan@sandia.gov>
2023-08-15 21:47:31 -0600

    more samplers to subdirs and per-dir man pages

---
+++ ldms/src/sampler/tsampler/cray_power_sampler.c
@@ -0,0 +1,269 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2016-2018 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2016-2018 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * \file cray_power_sampler.c
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include "ldms.h"
+#include "ldmsd.h"
+#include "timer_base.h"
+#include "sampler_base.h"
+
+static ovis_log_t mylog;
+
+typedef enum {
+	CPS_ENERGY,
+	/* CPS_FRESHNESS, */
+	/* CPS_GENERATION, */
+	CPS_POWER,
+	/* CPS_POWER_CAP, */
+	/* CPS_STARTUP, */
+	/* CPS_VERSION, */
+	CPS_LAST,
+} cps_metric_type;
+
+static const char *cps_names[] = {
+	[CPS_ENERGY]      =  "energy",
+	/* [CPS_FRESHNESS]   =  "freshness", */
+	/* [CPS_GENERATION]  =  "generation", */
+	[CPS_POWER]       =  "power",
+	/* [CPS_POWER_CAP]   =  "power_cap", */
+	/* [CPS_STARTUP]     =  "startup", */
+	/* [CPS_VERSION]     =  "version", */
+};
+
+static const char *cps_files[] = {
+	[CPS_ENERGY]      =  "/sys/cray/pm_counters/energy",
+	/* [CPS_FRESHNESS]   =  "/sys/cray/pm_counters/freshness", */
+	/* [CPS_GENERATION]  =  "/sys/cray/pm_counters/generation", */
+	[CPS_POWER]       =  "/sys/cray/pm_counters/power",
+	/* [CPS_POWER_CAP]   =  "/sys/cray/pm_counters/power_cap", */
+	/* [CPS_STARTUP]     =  "/sys/cray/pm_counters/startup", */
+	/* [CPS_VERSION]     =  "/sys/cray/pm_counters/version", */
+};
+
+struct cray_power_sampler {
+	struct timer_base base;
+	int fd[CPS_LAST];
+	struct timeval hfinterval;
+	int hfcount;
+};
+
+static const char *cray_power_sampler_usage(struct ldmsd_plugin *self)
+{
+	return  "config name=cray_power_sampler producer=<prod_name>"
+		" instance=<inst_name> [hfinterval=<hfinterval>] "
+		" [hfcount=<hfcount>] [component_id=<compid>] [schema=<sname>] "
+		" [with_jobid=(0|1)]\n"
+		"    <prod_name>  The producer name.\n"
+		"    <inst_name>  The instance name.\n"
+		"    <hfinterval> (Optional) the high-frequency interval (micro second, default: 100000).\n"
+		"    <hfcount>    (Optional) the number of elements in the ring buffer (default: 600).\n"
+		"    <compid>     (Optional) unique number identifier. Defaults to zero.\n"
+		"    <sname>      (Optional) schema name. Defaults to 'sampler_timer'.\n"
+		"    with_jobid   (Optional) enable(1) or disable(0) job info lookup (default: 1).\n"
+		;
+}
+
+static
+void cray_power_sampler_cleanup(struct cray_power_sampler *cps)
+{
+	int fd, i;
+	/* terminate timers */
+	timer_base_cleanup(&cps->base);
+	for (i = 0; i < CPS_LAST; i++) {
+		fd = cps->fd[i];
+		if (fd >= 0) {
+			close(fd);
+		}
+	}
+}
+
+static
+void cray_power_sampler_term(struct ldmsd_plugin *self)
+{
+	cray_power_sampler_cleanup((void*)self);
+	if (mylog)
+		ovis_log_destroy(mylog);
+}
+
+static
+void cray_power_sampler_timer_cb(tsampler_timer_t t)
+{
+	int rc;
+	int cps_idx;
+	off_t off;
+	struct cray_power_sampler *cps;
+	uint64_t v;
+	char buff[64];
+
+	/* t->sampler, t->set and t->mid (metric-id) are set when the time is
+	 * setup in create_metric_set().
+	 *
+	 * t->idx is the current index in the array provided by timer. This will
+	 * keep modulo-increasing call-after-call (per timer).
+	 * */
+
+	cps = (void*)t->sampler;
+
+	cps_idx = (uint64_t)t->ctxt; /* abuse ctxt */
+
+	off = lseek(cps->fd[cps_idx], 0, SEEK_SET);
+	if (off < 0) {
+		/* error */
+		ovis_log(mylog, OVIS_LDEBUG, "lseek() error, "
+				"errno: %d, cps_idx: %d\n.", errno, cps_idx);
+		return;
+	}
+
+	rc = read(cps->fd[cps_idx], buff, sizeof(buff));
+	if (rc < 0) {
+		/* error */
+		ovis_log(mylog, OVIS_LDEBUG, "read() error, "
+				"errno: %d, cps_idx: %d\n.", errno, cps_idx);
+		return;
+	}
+
+	v = strtoul(buff, NULL, 0);
+	ldms_metric_array_set_u64(t->set, t->mid, t->idx, v);
+}
+static
+int cray_power_sampler_config(struct ldmsd_plugin *self,
+				struct attr_value_list *kwl,
+				struct attr_value_list *avl)
+{
+	char *v;
+	uint64_t x;
+	int rc;
+	struct cray_power_sampler *cps = (void*)self;
+	int i;
+
+	rc = timer_base_config(self, kwl, avl, mylog);
+	if (rc) {
+		goto out;
+	}
+
+	/* set default values */
+	cps->hfinterval.tv_sec = 0;
+	cps->hfinterval.tv_usec = 100000;
+	cps->hfcount = 600;
+
+	v = av_value(avl, "hfinterval");
+	if (v) {
+		x = strtoull(v, NULL, 0);
+		if (!x) {
+			rc = EINVAL;
+			goto cleanup;
+		}
+		cps->hfinterval.tv_sec = x / 1000000;
+		cps->hfinterval.tv_usec = x % 1000000;
+	}
+
+	v = av_value(avl, "hfcount");
+	if (v) {
+		cps->hfcount = strtol(v, NULL, 0);
+		if (!cps->hfcount) {
+			rc = EINVAL;
+			goto cleanup;
+		}
+	}
+
+	for (i = 0; i < CPS_LAST; i++) {
+		cps->fd[i] = open(cps_files[i], O_RDONLY);
+		if (cps->fd[i] < 0)
+			goto cleanup;
+		rc = timer_base_add_hfmetric(&cps->base, cps_names[i],
+				LDMS_V_U64_ARRAY, cps->hfcount,
+				&cps->hfinterval, cray_power_sampler_timer_cb,
+				(void*)(uint64_t)i);
+		if (rc)
+			goto cleanup;
+	}
+
+	rc = timer_base_create_set(&cps->base);
+	if (rc) {
+		goto cleanup;
+	}
+	goto out;
+
+cleanup:
+	timer_base_cleanup(&cps->base);
+out:
+	return rc;
+}
+struct ldmsd_plugin *get_plugin()
+{
+	int rc;
+	mylog = ovis_log_register("sampler.cray_power_sampler", "The log subsystem of the cray_power_sampler plugin");
+	if (!mylog) {
+		rc = errno;
+		ovis_log(NULL, OVIS_LWARN, "Failed to create the subsystem "
+				"of 'cray_power_sampler' plugin. Error %d\n", rc);
+	}
+	struct cray_power_sampler *cps = calloc(1, sizeof(*cps));
+
+	if (!cps)
+		return NULL;
+
+	timer_base_init(&cps->base);
+	/* override */
+	cps->base.base.base.usage = cray_power_sampler_usage;
+	cps->base.base.base.term = cray_power_sampler_term;
+	cps->base.base.base.config = cray_power_sampler_config;
+	snprintf(cps->base.base.base.name, sizeof(cps->base.base.base.name),
+			"cray_power_sampler");
+
+	return (void*)cps;
+}
---
+++ ldms/src/sampler/tsampler/timer_base.c
@@ -0,0 +1,351 @@
+/* -*- c-basic-offset: 8 -*-
+ * Copyright (c) 2016-2018 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * Copyright (c) 2016-2018 Open Grid Computing, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the BSD-type
+ * license below:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *      Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *      Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *      Neither the name of Sandia nor the names of any contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ *      Neither the name of Open Grid Computing nor the names of any
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *      Modified source versions must be plainly marked as such, and
+ *      must not be misrepresented as being the original software.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file timer_base.c
+ * \brief a base class of ldmsd sampler with additional timer.
+ *
+ * This is a base class of ldmsd sampler that uses tsampler utility (see \ref
+ * tsampler.h and \ref tsampler.c) as additional timer to sample high-frequency
+ * data.
+ *
+ * This sampler plugin will create metric arrays of timestamps, each array
+ * associated with the 'interval' parameter supplied in the configuration (see
+ * usage() and config()). The arrays then populated with 64-bit timestamp values
+ * [(high_32_bit) sec | (low_32_bit) usec].
+ *
+ * The extra-timer is setup in create_metric_set(), and are started in sample().
+ */
+
+#include <assert.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/queue.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include "ldms.h"
+#include "ldmsd.h"
+#include "tsampler.h"
+#include "timer_base.h"
+#include "ldms_jobid.h"
+
+/* state of the sampler */
+typedef enum {
+	TBS_INIT,
+	TBS_CONFIGURED,
+	TBS_RUNNING,
+} timer_base_state_e;
+
+/* Convenient functions to convert timeval <--> u64 */
+static inline
+struct timeval u64_to_tv(uint64_t x)
+{
+	struct timeval tv;
+	tv.tv_sec = x >> 32;
+	tv.tv_usec = x & 0xFFFFFFFF;
+	return tv;
+}
+
+static inline
+uint64_t tv_to_u64(struct timeval tv)
+{
+	uint64_t x;
+	x = (((uint64_t)tv.tv_sec) << 32) | (tv.tv_usec & 0xFFFFFFFF);
+	return x;
+}
+
+ldms_set_t timer_base_get_set(struct ldmsd_sampler *self)
+{
+	struct timer_base *tb = (void*)self;
+	return tb->set;
+}
+
+int timer_base_create_metric_set(struct timer_base *tb)
+{
+	int rc = 0;
+	pthread_mutex_lock(&tb->mutex);
+	tb->set = ldms_set_new(tb->iname, tb->schema);
+	if (!tb->set) {
+		rc = errno;
+		goto out;
+	}
+	ldms_metric_set_u64(tb->set, 0, tb->compid);
+	ldms_metric_set_u64(tb->set, 1, 0); /* default jobid */
+	tb->state = TBS_CONFIGURED;
+out:
+	pthread_mutex_unlock(&tb->mutex);
+	return rc;
+}
+
+int timer_base_add_hfmetric(struct timer_base *tb,
+				const char *name,
+				enum ldms_value_type type,
+				int n,
+				const struct timeval *interval,
+				tsampler_sample_cb cb,
+				void *ctxt)
+{
+	int rc = 0;
+	struct tsampler_timer_entry *t = calloc(1, sizeof(*t));
+	if (!t) {
+		rc = errno;
+		goto out;
+	}
+	if (type < LDMS_V_CHAR_ARRAY || LDMS_V_D64_ARRAY < type) {
+		rc = EINVAL;
+		goto out;
+	}
+	t->timer.n = n;
+	t->timer.mid = ldms_schema_metric_array_add(tb->schema, name, type, t->timer.n);
+	if (t->timer.mid < 0) {
+		rc = -t->timer.mid;
+		goto out;
+	}
+	snprintf(tb->buff, sizeof(tb->buff), "%s_timeval", name);
+	t->timer.tid = ldms_schema_metric_array_add(tb->schema, tb->buff,
+				LDMS_V_U64_ARRAY, t->timer.n*2);
+	if (t->timer.tid < 0) {
+		rc = -t->timer.tid;
+		goto out;
+	}
+	t->timer.sampler = &tb->base;
+	t->timer.cb = cb;
+	t->timer.ctxt = ctxt;
+	t->timer.interval = *interval;
+	TAILQ_INSERT_TAIL(&tb->timer_list, t, entry);
+	/* timer will be activated later in sample() function */
+out:
+	if (rc && t) {
+		free(t);
+	}
+	return rc;
+}
+
+int timer_base_config(struct ldmsd_plugin *self, struct attr_value_list *kwl,
+		      struct attr_value_list *avl, ovis_log_t mylog)
+{
+	struct timer_base *tb;
+	int rc = 0;
+
+	tb = (void*)self;
+
+	tb->mylog = mylog;
+
+	pthread_mutex_lock(&tb->mutex);
+	switch (tb->state) {
+	case TBS_INIT:
+		/* OK */
+		break;
+	case TBS_CONFIGURED:
+	case TBS_RUNNING:
+		rc = EEXIST;
+		goto out;
+	}
+
+	if (tb->schema) {
+		ovis_log(tb->mylog, OVIS_LERROR, "%s: schema existed.\n", tb->base.base.name);
+		rc = EEXIST;
+		goto out;
+	}
+
+	if (tb->set) {
+		ovis_log(tb->mylog, OVIS_LERROR, "%s: set existed.\n", tb->base.base.name);
+		rc = EEXIST;
+		goto out;
+	}
+	tb->cfg = base_config(avl, tb->base.base.name, tb->base.base.name, mylog);
+	if (!tb->cfg) {
+		rc = errno;
+		goto out;
+	}
+
+
+	snprintf(tb->pname, sizeof(tb->pname), "%s", tb->cfg->producer_name);
+	snprintf(tb->iname, sizeof(tb->iname), "%s", tb->cfg->instance_name);
+	tb->compid = tb->cfg->component_id;
+	tb->schema = base_schema_new(tb->cfg);
+	if (!tb->schema) {
+		rc = errno;
+		goto out;
+	}
+
+	rc = 0;
+	tb->state = TBS_CONFIGURED;
+
+out:
+	pthread_mutex_unlock(&tb->mutex);
+	return rc;
+}
+
+int timer_base_create_set(struct timer_base *tb)
+{
+	tb->set = base_set_new(tb->cfg);
+	if (!tb->set) {
+		ovis_log(tb->mylog, OVIS_LERROR, "%s: ldms_set_new() failed, errno: %d.\n",
+				tb->base.base.name, errno);
+		return errno;
+	}
+	return 0;
+}
+
+void timer_base_cleanup(struct timer_base *tb);
+
+void timer_base_term(struct ldmsd_plugin *self)
+{
+	/* remove all timers when we terminate */
+	timer_base_cleanup((void*)self);
+}
+
+int timer_base_sample(struct ldmsd_sampler *self)
+{
+	struct timer_base *tb = (void*)self;
+	int rc = 0;
+	struct tsampler_timer_entry *ent;
+
+
+	pthread_mutex_lock(&tb->mutex);
+	base_sample_begin(tb->cfg);
+	switch (tb->state) {
+	case TBS_INIT:
+		assert(0);
+		break;
+	case TBS_CONFIGURED:
+		/* add timers if this is the first sample() */
+		TAILQ_FOREACH(ent, &tb->timer_list, entry) {
+			ent->timer.set = tb->set;
+			rc = tsampler_timer_add(&ent->timer);
+			if (rc)
+				goto timer_cleanup;
+		}
+		tb->state = TBS_RUNNING;
+		break;
+	case TBS_RUNNING:
+		/* do nothing */
+		break;
+	}
+	base_sample_end(tb->cfg);
+	pthread_mutex_unlock(&tb->mutex);
+
+	goto out;
+
+timer_cleanup:
+	/* remove added timers at cleanup */
+	while ((ent = TAILQ_FIRST(&tb->timer_list))) {
+		TAILQ_REMOVE(&tb->timer_list, ent, entry);
+		tsampler_timer_remove(&ent->timer);
+	}
+	pthread_mutex_unlock(&tb->mutex);
+
+out:
+	ldms_transaction_end(tb->set);
+	return rc;
+}
+
+const char *timer_base_usage(struct ldmsd_plugin *self)
+{
+	return "timer_base is a base-class sampler that cannot be used by itself.";
+}
+
+void timer_base_remove_timers(struct timer_base *tb)
+{
+	struct tsampler_timer_entry *ent;
+	while ((ent = TAILQ_FIRST(&tb->timer_list))) {
+		TAILQ_REMOVE(&tb->timer_list, ent, entry);
+		tsampler_timer_remove(&ent->timer);
+		free(ent);
+	}
+}
+
+void timer_base_cleanup(struct timer_base *tb)
+{
+	timer_base_remove_timers(tb);
+	if (tb->set) {
+		ldms_set_delete(tb->set);
+		tb->set = NULL;
+	}
+	if (tb->cfg) {
+		base_del(tb->cfg);
+		tb->cfg = NULL;
+	}
+	tb->schema = NULL;
+	tb->state = TBS_INIT;
+}
+
+static
+int __config(struct ldmsd_plugin *self, struct attr_value_list *kwl,
+struct attr_value_list *avl)
+{
+	assert(0 == "ERROR timer_base.config() not overridden.");
+	return ENOSYS;
+}
+
+void timer_base_init(struct timer_base *tb)
+{
+	/* sub-class can override these values after calling this function */
+	snprintf(tb->base.base.name, sizeof(tb->base.base.name), "timer_base");
+	tb->base.base.type = LDMSD_PLUGIN_SAMPLER;
+	tb->base.base.term = timer_base_term;
+	tb->base.base.config = __config;
+	tb->base.base.usage = timer_base_usage;
+	tb->base.get_set = timer_base_get_set;
+	tb->base.sample = timer_base_sample;
+	tb->state = TBS_INIT;
+	TAILQ_INIT(&tb->timer_list);
+	pthread_mutex_init(&tb->mutex, NULL);
+}
+
+struct ldmsd_plugin *get_plugin()
+{
+	struct timer_base *tb = calloc(1, sizeof(*tb));
+	if (!tb)
+		return NULL;
+	timer_base_init(tb);
+	return (void*)tb;
+}
