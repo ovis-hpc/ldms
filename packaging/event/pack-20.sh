e12b1c99319064de36c654f5634d68d0af955465
Christopher J. Morrone <morrone2@llnl.gov>
2025-02-18 10:22:10 -0800

    Remove ancient packaging scripts dir packaging/

--- packaging/event/pack-20.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#echo cleaning rpms dir
-#/bin/rm -f ../RPMS/*/libevent*.rpm
-#echo cleaning srpm dir
-#/bin/rm -f ../SRPMS/libevent*.src.rpm
-script -c 'rpmbuild -ba libevent20ovis.spec' packlibevent2
