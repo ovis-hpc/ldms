30dd7ef9b5421b007d914cbfd80ba208a23fc112
Christopher J. Morrone <morrone2@llnl.gov>
2025-05-29 16:47:03 -0700

    Improve rst2man detection and build messages
    Allow the RST2MAN variable to point to be used in rules.mk.
    
    Add AM_V_P logic that allows both quiet and verbose output when generating
    man pages.
    
    Fixes #1760

--- ldms/rules.mk
+++ ldms/rules.mk
@@ -4,10 +4,14 @@
 # 	Ex: \fB\-a,\fP\fI\-\-default_auth\fP --> \fB-a,\fP\fI--default_auth\fP
 # 3. Remove lines with unnecessary rst2man output.
 # 	Ex: .de1 rstReportMargin, \\$1 \\n[an-margin], level \\n[rst2man-indent-level], .de1 INDENT,. RS \\$1, .de UNINDENT
+# The AM_V_P logic allows the output to be quiet when using "configure --enable-silent-rules"
+# or "make V=0" (default behavior), and be verbose when using "configure --disable-silent-rules"
+# or "make V=1".
 %.man: %.rst
-	@echo "Generating $@..."
-	@mkdir -p $(dir $@)
-	@sed -e 's/:ref:`\([^`]*\)<[^`]*>`/\1/g' $< | rst2man | sed -e 's/\\\([`'\''\-]\)/\1/g' \
+	@set -e; \
+	if $(AM_V_P); then set -x; else echo "  RST2MAN  $@"; fi; \
+	mkdir -p $(dir $@); \
+	sed -e 's/:ref:`\([^`]*\)<[^`]*>`/\1/g' $< | @RST2MAN@ | sed -e 's/\\\([`'\''\-]\)/\1/g' \
 		-e '/rst2man/d' \
 		-e '/rstR/d' \
 		-e '/. RS/d' \
