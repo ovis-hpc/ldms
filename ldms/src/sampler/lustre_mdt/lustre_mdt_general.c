c350ec4cdf96999a6108aa2f32e37625e9d40f42
Christopher J. Morrone <morrone2@llnl.gov>
2025-06-24 14:09:30 -0700

    lustre: Share updated stats reading function
    Backport of 83893e9b5ed1e7cd84d8f933ededc93820a186b8
    
    We backport the new implementation of the stats reading function
    into each of the three lustre samplers, but we don't share the
    function like on main. The sharing involved more moving around of
    code than we want to make on b4.4.

--- ldms/src/sampler/lustre_mdt/lustre_mdt_general.c
+++ ldms/src/sampler/lustre_mdt/lustre_mdt_general.c
@@ -15,6 +15,8 @@
 #include "lustre_mdt.h"
 #include "lustre_mdt_general.h"
 
+#define MAXNAMESIZE 64
+
 static ldms_schema_t mdt_general_schema;
 
 static char *mdt_md_stats_uint64_t_entries[] = {
@@ -244,77 +246,67 @@ ldms_set_t mdt_general_create(const char *producer_name,
         return set;
 }
 
-static void mdt_md_stats_sample(const char *stats_path,
-                                   ldms_set_t general_metric_set)
+static int mdt_md_stats_sample(const char *stats_path,
+			       ldms_set_t metric_set)
 {
-        FILE *sf;
-        char buf[512];
-        char str1[64+1];
+	FILE *sf;
+	char buf[512];
+	int rc = 0;
 
-        sf = fopen(stats_path, "r");
-        if (sf == NULL) {
-                log_fn(LDMSD_LWARNING, SAMP" file %s not found\n",
-                       stats_path);
-                return;
-        }
+	sf = fopen(stats_path, "r");
+	if (sf == NULL) {
+		log_fn(LDMSD_LWARNING, ": file %s not found\n",
+			 stats_path);
+		return ENOENT;
+	}
 
-        /* The first line should always be "snapshot_time"
+	/* The first line should always be "snapshot_time"
            we will ignore it because it always contains the time that we read
            from the file, not any information about when the stats last
            changed */
-        if (fgets(buf, sizeof(buf), sf) == NULL) {
-                log_fn(LDMSD_LWARNING, SAMP" failed on read from %s\n",
-                       stats_path);
-                goto out1;
-        }
-        if (strncmp("snapshot_time", buf, sizeof("snapshot_time")-1) != 0) {
-                log_fn(LDMSD_LWARNING, SAMP" first line in %s is not \"snapshot_time\": %s\n",
-                       stats_path, buf);
-                goto out1;
-        }
+	if (fgets(buf, sizeof(buf), sf) == NULL) {
+		log_fn(LDMSD_LWARNING, ": failed on read from %s\n",
+			 stats_path);
+		rc = ENOMSG;
+		goto out1;
+	}
+	if (strncmp("snapshot_time", buf, sizeof("snapshot_time")-1) != 0) {
+		log_fn(LDMSD_LWARNING, ": first line in %s is not \"snapshot_time\": %s\n",
+			 stats_path, buf);
+		rc = ENOMSG;
+		goto out1;
+	}
 
-        ldms_transaction_begin(general_metric_set);
-        while (fgets(buf, sizeof(buf), sf)) {
-                uint64_t val1, val2;
-                int rc;
-                int index;
+	while (fgets(buf, sizeof(buf), sf)) {
+		char field_name[MAXNAMESIZE+1];
+		uint64_t samples, sum;
+		int num_matches;
+		int index;
 
-                rc = sscanf(buf, "%64s %lu samples [%*[^]]] %*u %*u %lu",
-                            str1, &val1, &val2);
-                if (rc == 2) {
-                        index = ldms_metric_by_name(general_metric_set, str1);
-                        if (index == -1) {
-                                log_fn(LDMSD_LWARNING, SAMP" mdt md_stats metric not found: %s\n",
-                                       str1);
-                        } else {
-                                ldms_metric_set_u64(general_metric_set, index, val1);
-                        }
-                        continue;
-                } else if (rc == 3) {
-                        int base_name_len = strlen(str1);
-                        index = ldms_metric_by_name(general_metric_set, str1);
-                        if (index == -1) {
-                                log_fn(LDMSD_LWARNING, SAMP" mdt md_stats metric not found: %s\n",
-                                       str1);
-                        } else {
-                                ldms_metric_set_u64(general_metric_set, index, val1);
-                        }
-                        sprintf(str1+base_name_len, ".sum"); /* append ".sum" */
-                        index = ldms_metric_by_name(general_metric_set, str1);
-                        if (index == -1) {
-                                log_fn(LDMSD_LWARNING, SAMP" mdt md_stats metric not found: %s\n",
-                                       str1);
-                        } else {
-                                ldms_metric_set_u64(general_metric_set, index, val2);
-                        }
-                        continue;
-                }
-        }
-        ldms_transaction_end(general_metric_set);
+		num_matches = sscanf(buf,
+				     "%64s %"SCNu64" samples [%*[^]]] %*u %*u %"SCNu64" %*u",
+				     field_name, &samples, &sum);
+		if (num_matches >= 2) {
+			/* we know at least "samples" is available */
+			index = ldms_metric_by_name(metric_set, field_name);
+			if (index != -1) {
+				ldms_metric_set_u64(metric_set, index, samples);
+			}
+		}
+		if (num_matches >= 3) {
+			/* we know that "sum" is also avaible */
+			int base_name_len = strlen(field_name);
+			sprintf(field_name+base_name_len, ".sum"); /* append ".sum" */
+			index = ldms_metric_by_name(metric_set, field_name);
+			if (index != -1) {
+				ldms_metric_set_u64(metric_set, index, sum);
+			}
+		}
+	}
 out1:
-        fclose(sf);
+	fclose(sf);
 
-        return;
+	return rc;
 }
 
 void mdt_general_sample(const char *mdt_name, const char *stats_path,
@@ -322,6 +314,8 @@ void mdt_general_sample(const char *mdt_name, const char *stats_path,
 {
         log_fn(LDMSD_LDEBUG, SAMP" mdt_general_sample() %s\n",
                mdt_name);
-        mdt_md_stats_sample(stats_path, general_metric_set);
+	ldms_transaction_begin(general_metric_set);
+	mdt_md_stats_sample(stats_path, general_metric_set);
         osd_sample(osd_path, general_metric_set);
+	ldms_transaction_end(general_metric_set);
 }
