78acd62d115cb56d86a543e279c10aa0fad93720
Sara Walton <spwalto@sandia.gov>
2025-07-28 14:12:35 -0600

    changing ldmsd_plugin to ldmsd_store

--- ldms/src/store/kokkos_appmon/kokkos_appmon.c
+++ ldms/src/store/kokkos_appmon/kokkos_appmon.c
@@ -435,7 +435,7 @@ static void destructor(ldmsd_plug_handle_t handle)
         free(plugin_config_name);
 }
 
-struct ldmsd_plugin ldmsd_plugin_interface = {
+struct ldmsd_store ldmsd_plugin_interface = {
 	.base = {
 		.type = LDMSD_PLUGIN_STORE,
 		.config = config,
