bf08c472794af53e1fecb68cbb10126a4dcc5461
Ben Allan <baallan@sandia.gov>
2025-08-25 11:46:04 -0600

    fix double free of json parser in resp_stream_status of ldmsctl

--- ldms/src/ldmsd/ldmsctl.c
+++ ldms/src/ldmsd/ldmsctl.c
@@ -1847,9 +1847,9 @@ static void resp_stream_status(ldmsd_req_hdr_t resp, size_t len,
 	}
 	rc = json_parse_buffer(parser, (char*)attr->attr_value, len, &json);
 	json_parser_free(parser);
+	parser = NULL;
 	if (rc) {
-		printf("syntax error parsing JSON string\n");
-		json_parser_free(parser);
+		printf("syntax error parsing JSON string: %s\n", (char*)attr->attr_value);
 		return;
 	}
 
