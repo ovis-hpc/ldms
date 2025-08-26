f2cee69a1e6052f1dc6c18222d79a3e489c1ccd3
Christopher J. Morrone <morrone2@llnl.gov>
2025-08-26 13:10:49 -0700

    Make ldmsd_plug_handle_t a pointer to an opaque struct
    Convert ldmsd_plug_handle_t from a a void pointer to a pointer
    to an opaque struct. The new new opaque struct ldmsd_plug_api_s
    is initially implmented as a direct derivative of struct ldmsd_cfgobj.
    
    This improves type checking on the consumer (plugin) side of the API.

---
+++ ldms/src/ldmsd/ldmsd_plug_api_internal.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 Lawrence Livermore National Security, LLC
+ * See the top-level COPYING file for details.
+ *
+ * SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+ */
+#ifndef _LDMSD_PLUG_API_INTERNAL_H
+#define _LDMSD_PLUG_API_INTERNAL_H
+
+#include "ldmsd.h"
+
+struct ldmsd_plug_api_s {
+	struct ldmsd_cfgobj cfgobj;
+};
+
+#endif
