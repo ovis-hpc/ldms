0bac91e1f0b5609d2dbebaf28e9ce3ee2635b8f7
Christopher J. Morrone <morrone2@llnl.gov>
2025-08-26 17:53:09 -0700

    lustre samplers: move cid structs into per-instance context

--- ldms/src/sampler/lustre_newer/lustre_client_general.h
+++ ldms/src/sampler/lustre_newer/lustre_client_general.h
@@ -9,7 +9,6 @@
 
 #include "ldms.h"
 #include "ldmsd.h"
-#include "comp_id_helper.h"
 #include "sampler_base.h"
 #include "lustre_client.h"
 
@@ -23,13 +22,12 @@ int llite_general_schema_is_initialized();
  * are defined as lustre_client_%d, with the value of schema_extras,
  * unless schema_extras==0.
  */
-int llite_general_schema_init(lc_context_t ctxt, comp_id_t cid, int schema_extras);
+int llite_general_schema_init(lc_context_t ctxt, int schema_extras);
 void llite_general_schema_fini(lc_context_t ctxt);
 ldms_set_t llite_general_create(lc_context_t ctxt,
 				const char *producer_name,
                                 const char *fs_name,
                                 const char *llite_name,
-				const comp_id_t cid,
 				const struct base_auth *auth);
 void llite_general_sample(lc_context_t ctxt,
 			  const char *llite_name, const char *stats_path,
