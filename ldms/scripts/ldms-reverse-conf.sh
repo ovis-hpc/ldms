745113b8e2ff8a86d7dc6742ab4a25b630d6d961
Ben Allan <baallan@sandia.gov>
2025-10-10 00:32:17 -0600

    add pid pipeline test cases and a log line format fix in linux_proc_sampler.c
    The pipeline is:
    - ldms-notify (netlink.c) publishes to L0
    - linux_proc_sampler on L0 subscribes to pids and for each:
      - emits messages for argv, open files, and environment of the pid
      - creates a metric set for the pid
      - destroys the metric set for the pid when the pid is gone
    - blob_msg_writer on L1 subscribes to all the channels, creating store/blob/* files
    - csv store on L2 writes out linux_proc_sampler data and dstat data
    
    the dstat data can be checked for fd growth if the run time is extended.
    
    the tests are best run  (after install) with
    
    % become root, then
    script -c 'allowroot=1 $prefix/bin/ldms-static-test.sh CASE' log.case
    where CASE is one of:
    - linux_proc_sampler
       (base case: often crashes per #2034)
    - linux_proc_sampler_memcheck
       (documents the crash per #2034)
    - linux_proc_sampler_drd
       (documents a mess of race conditions in L1)
    - linux_proc_sampler_no_teardown
       (test case for tinkering that omits commands to teardown objects before exit)

--- ldms/scripts/ldms-reverse-conf.sh
+++ ldms/scripts/ldms-reverse-conf.sh
@@ -5,10 +5,12 @@ grep -v '^# ' | \
 grep -v 'option ' | \
 grep -v 'listen ' | \
 grep -v 'config ' | \
+grep -v 'msg_enable' | \
+grep -v 'stream_enable' | \
 sed -e 's/strgp_start/strgp_stop/' | \
 sed -e 's/strgp_prdcr_add/strgp_prdcr_del/' | \
 sed -e 's/\(strgp_add\)\( *\)\(name=\)\([a-zA-Zi0-9{}$._-]*\)\(.*\)/strgp_del \3\4/' | \
-sed -e 's/load /term /' | \
+sed -e 's/\(load\)\(.*\)/stop \2\nterm \2/' | \
 sed -e 's/\(updtr_start\)\( *\)\(name=\)\([a-zA-Zi0-9{}$._-]*\)\(.*\)/updtr_stop \3\4/' | \
 sed -e 's/updtr_match_add/updtr_match_del/' | \
 sed -e 's/updtr_prdcr_add/updtr_prdcr_del/' | \
