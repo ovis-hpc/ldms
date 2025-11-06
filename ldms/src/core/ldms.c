72889507174280a61fb07d77e7a37414065a1510
Tom Tucker <tom@ogc.us>
2025-11-05 17:15:09 -1000

    Fix memory leak in ldms_schema_delete()
    ldms_schema_delete was calling ldms_record_delete, however,
    this function checks if the record is already a member of a
    schema and if it is, refuses to delete it. This resulted in
    a memory leak in ldms_schema_delete().

--- ldms/src/core/ldms.c
+++ ldms/src/core/ldms.c
@@ -1312,13 +1312,9 @@ int ldms_schema_fprint(ldms_schema_t schema, FILE *fp)
 	return 0;
 }
 
-void ldms_record_delete(ldms_record_t rec_def)
+static void __record_delete(ldms_record_t rec_def)
 {
 	ldms_mdef_t m;
-	if (!rec_def)
-		return;
-	if (rec_def->metric_id >= 0)
-		return; /* This already belonged to a schema */
 	while ((m = STAILQ_FIRST(&rec_def->rec_metric_list))) {
 		STAILQ_REMOVE_HEAD(&rec_def->rec_metric_list, entry);
 		free(m->name);
@@ -1328,6 +1324,15 @@ void ldms_record_delete(ldms_record_t rec_def)
 	free(rec_def->mdef.name);
 	free(rec_def->mdef.unit);
 	free(rec_def);
+}
+
+void ldms_record_delete(ldms_record_t rec_def)
+{
+	if (!rec_def)
+		return;
+	if (rec_def->metric_id >= 0)
+		return; /* This already belonged to a schema */
+	__record_delete(rec_def);
 }
 
 void ldms_schema_delete(ldms_schema_t schema)
@@ -1341,7 +1346,7 @@ void ldms_schema_delete(ldms_schema_t schema)
 		m = STAILQ_FIRST(&schema->metric_list);
 		STAILQ_REMOVE_HEAD(&schema->metric_list, entry);
 		if (m->type == LDMS_V_RECORD_TYPE) {
-			ldms_record_delete((ldms_record_t)m);
+			__record_delete((ldms_record_t)m);
 			continue;
 		}
 		free(m->name);
