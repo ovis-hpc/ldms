3f2a33573e313b2063308a86712fe4f3bee06e38
Christopher J. Morrone <morrone2@llnl.gov>
2025-11-13 20:27:45 -0800

    Document struct ldmsd_plug_api_s

--- ldms/src/ldmsd/ldmsd_plug_api_internal.h
+++ ldms/src/ldmsd/ldmsd_plug_api_internal.h
@@ -9,6 +9,18 @@
 
 #include "ldmsd.h"
 
+/*
+ * struct ldmsd_plug_api_s is currently implemented as a type
+ * punning alias for struct ldmsd_cfgobj. This provides
+ * a unique compiler-recognizable type upon which to define the
+ * ldmsd_plug_handle_t opaque handle. It does this while keeping the
+ * real implmentation of the opaque handle (currently a
+ * struct ldmsd_cfgobj or one of its type-punned derivatives) opaque
+ * to the users of the plugin API.
+ *
+ * NOTE - The implication of the above is that nothing can be added
+ * to struct ldmsd_plug_api_s in its current implmentation!
+ */
 struct ldmsd_plug_api_s {
 	struct ldmsd_cfgobj cfgobj;
 };
